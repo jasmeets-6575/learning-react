79b8f3ca2163b304e1e33745725061a80a2159c9
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-01-16 20:20:20 +0530

    Booklist project adding CSS

--- By john Smilga/booklist/src/index.js
+++ By john Smilga/booklist/src/index.js
@@ -20,11 +20,21 @@ const Book = () => {
     </article>
 }
 
-const Image = () => <img src="https://m.media-amazon.com/images/I/51oVTRsjcqL._AC_UY218_.jpg" alt="THE SILENT PATIENT"></img>
+const Image = () => <img src="https://m.media-amazon.com/images/I/51oVTRsjcqL._SX329_BO1,204,203,200_.jpg" alt="THE SILENT PATIENT"></img>
 const Title = () => <h2>THE SILENT PATIENT</h2>
+
 const Author = () => {
-    return <h2>Alex Michaelides</h2>
+    return <h2 style={{color:'#617d98',fontSize:'1rem',marginTop:'0.5rem'}}> Alex Michaelides</h2>
 }
+
+// const Author = () => {
+//     const inlineHeadingStyle = {
+//         color:'#617d98',
+//         fontSize:'1rem',
+//         marginTop:'0.5rem'        
+//     };
+//     return <h4 style={inlineHeadingStyle} >Alex Michaelides</h4>
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
