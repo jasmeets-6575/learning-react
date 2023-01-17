6c95d0d1d7bf74e0c4eaa96e08e301dd8f1645f8
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-01-17 20:20:20 +0530

    Booklist project adding JS

--- By john Smilga/booklist/src/index.js
+++ By john Smilga/booklist/src/index.js
@@ -12,20 +12,16 @@ function Booklist(){
     </section>
 }
 
+const author = "Alex Michaelides";
 const Book = () => {
+    const title = "THE SILENT PATIENT";
     return <article className="book">
-        <Image/>
-        <Title/>
-        <Author/>
+        <img src="https://m.media-amazon.com/images/I/51oVTRsjcqL._SX329_BO1,204,203,200_.jpg" alt="THE SILENT PATIENT"></img>
+        <h2>{title}</h2>
+        <h4>{author.toUpperCase()}</h4>
     </article>
 }
 
-const Image = () => <img src="https://m.media-amazon.com/images/I/51oVTRsjcqL._SX329_BO1,204,203,200_.jpg" alt="THE SILENT PATIENT"></img>
-const Title = () => <h2>THE SILENT PATIENT</h2>
-
-const Author = () => {
-    return <h2 style={{color:'#617d98',fontSize:'1rem',marginTop:'0.5rem'}}> Alex Michaelides</h2>
-}
 
 // const Author = () => {
 //     const inlineHeadingStyle = {
