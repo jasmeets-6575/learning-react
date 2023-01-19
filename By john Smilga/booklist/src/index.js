68ae7bf54466cc6e13c76b2a35c98794f814164f
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-01-19 20:20:20 +0530

    Booklist project adding prop

--- By john Smilga/booklist/src/index.js
+++ By john Smilga/booklist/src/index.js
@@ -1,35 +1,58 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import "./index.css"
+import "./index.css";
 
 const firstBook = {
-    author:"Alex Michaelides",
-    title:"THE SILENT PATIENT",
-    img:"https://m.media-amazon.com/images/I/51oVTRsjcqL._SX329_BO1,204,203,200_.jpg"
+  author: "Alex Michaelides",
+  title: "THE SILENT PATIENT",
+  img: "https://m.media-amazon.com/images/I/51oVTRsjcqL._SX329_BO1,204,203,200_.jpg",
 };
 
 const secondBook = {
-    author:"Oishik Majumdars",
-    title:"Dhusar Jagat",
-    img:"https://m.media-amazon.com/images/I/419UfMbI55L._SX319_BO1,204,203,200_.jpg"
+  author: "Oishik Majumdars",
+  title: "Dhusar Jagat",
+  img: "https://m.media-amazon.com/images/I/419UfMbI55L._SX319_BO1,204,203,200_.jpg",
 };
 
-function Booklist(){
-    return <section className="booklist">
-        <Book author={firstBook.author} title={firstBook.title} img={firstBook.img} />
-        <Book author={secondBook.author} title={secondBook.title} img={secondBook.img} />
+const thirdBook = {
+  author: "Will Smith",
+  title: "Will",
+  img: "https://m.media-amazon.com/images/I/51povAtTE5L._SX327_BO1,204,203,200_.jpg",
+};
+
+function Booklist() {
+  return (
+    <section className="booklist">
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        img={firstBook.img}
+      />
+      <Book
+        author={secondBook.author}
+        title={secondBook.title}
+        img={secondBook.img}
+      />
+      <Book
+        author={thirdBook.author}
+        title={thirdBook.title}
+        img={thirdBook.img}
+      />
     </section>
+  );
 }
 
 const Book = (props) => {
-    return (<article className="book">
-        <img src={props.img} alt={props.title}></img>
-        <h2>{props.title}</h2>
-        <h4>{props.author}</h4>
+  const { img, title, author } = props;
+  return (
+    <article className="book">
+      <img src={img} alt={title}></img>
+      <h2>{title}</h2>
+      <h4>{author}</h4>
     </article>
-    );
+  );
 };
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Booklist/>);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Booklist />);
