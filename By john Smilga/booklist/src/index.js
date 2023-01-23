d01f740368761be0185429a6ee43ba4d630694d5
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-01-23 22:23:21 +0530

    Booklist project adding form

--- By john Smilga/booklist/src/index.js
+++ By john Smilga/booklist/src/index.js
@@ -8,31 +8,62 @@ const books = [
     author: "Alex Michaelides",
     title: "THE SILENT PATIENT",
     img: "https://m.media-amazon.com/images/I/51oVTRsjcqL._SX329_BO1,204,203,200_.jpg",
-    id:1,
+    id: 1,
   },
   {
     author: "Oishik Majumdars",
     title: "Dhusar Jagat",
     img: "https://m.media-amazon.com/images/I/419UfMbI55L._SX319_BO1,204,203,200_.jpg",
-    id:2,
+    id: 2,
   },
   {
     author: "Will Smith",
     title: "Will",
     img: "https://m.media-amazon.com/images/I/51povAtTE5L._SX327_BO1,204,203,200_.jpg",
-    id:3,
+    id: 3,
   },
 ];
 
 function Booklist() {
   return (
     <section className="booklist">
+      <EventExamples />
       {books.map((book) => {
         return <Book {...book} key={book.id} />;
       })}
     </section>
   );
 }
+
+const EventExamples = () => {
+  const handleFormInput = (e) => {
+    console.log(e.target);
+    console.log(e.target.name);
+    console.log(e.target.value);
+  };
+  const handleButtonClick = () => {
+    alert("handle button click");
+  };
+  const handleFormSubmission = (e) => {
+    e.preventDefault();
+    console.log("Form Submitted");
+  };
+
+  return (
+    <section>
+      <form onSubmit={handleFormSubmission}>
+        <h2>Typical form</h2>
+        <input
+          type="text"
+          name="example"
+          onChange={handleFormInput}
+          style={{ margin: "1rem 0" }}
+        />
+      </form>
+      <button onClick={handleButtonClick}>Click Me</button>
+    </section>
+  );
+};
 
 const Book = (props) => {
   const { img, title, author } = props;
