ad3247378558945bfe6229b00fbce27998e30713
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-01-26 22:23:21 +0530

    Booklist project adding component dynamicaly

--- By john Smilga/booklist/src/index.js
+++ By john Smilga/booklist/src/index.js
@@ -27,7 +27,6 @@ const books = [
 function Booklist() {
   return (
     <section className="booklist">
-      <EventExamples />
       {books.map((book) => {
         return <Book {...book} key={book.id} />;
       })}
@@ -35,42 +34,53 @@ function Booklist() {
   );
 }
 
-const EventExamples = () => {
-  
-  const handleButtonClick = () => {
-    alert("handle button click");
-  };
-  const handleFormSubmission = (e) => {
-    e.preventDefault();
-    console.log("Form Submitted");
-  };
-
-  return (
-    <section>
-      <form >
-        <h2>Typical form</h2>
-        <input
-          type="text"
-          name="example"
-          onChange={(e=> console.log(e.target.value) )}
-          style={{ margin: "1rem 0" }}
-         />
-      </form>
-      <button onClick={()=> console.log("click me")}>Click Me</button>
-    </section>
-  );
-};
 
 const Book = (props) => {
   const { img, title, author } = props;
+  const displayTitle = () => {
+    console.log(title);
+  }
   return (
     <article className="book">
       <img src={img} alt={title}></img>
       <h2>{title}</h2>
+      <button onClick={displayTitle}>Display Title</button>
       <h4>{author}</h4>
     </article>
   );
 };
+
+
+
+
+
+
+// const EventExamples = () => {
+  
+//   const handleButtonClick = () => {
+//     alert("handle button click");
+//   };
+//   const handleFormSubmission = (e) => {
+  //     e.preventDefault();
+  //     console.log("Form Submitted");
+  //   };
+
+//   return (
+//     <section>
+//       <form >
+//         <h2>Typical form</h2>
+//         <input
+//           type="text"
+//           name="example"
+//           onChange={(e=> console.log(e.target.value) )}
+//           style={{ margin: "1rem 0" }}
+//          />
+//       </form>
+//       <button onClick={()=> console.log("click me")}>Click Me</button>
+//     </section>
+//   );
+// };
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Booklist />);
