438bf605b733640b7e6be631f2ecb5e9aef1b593
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-01-30 22:23:21 +0530

    Booklist project splitting data

---
+++ By john Smilga/booklist/src/books.js
@@ -0,0 +1,21 @@
+export const books = [
+    {
+      author: "Alex Michaelides",
+      title: "THE SILENT PATIENT",
+      img: "https://m.media-amazon.com/images/I/51oVTRsjcqL._SX329_BO1,204,203,200_.jpg",
+      id: 1,
+    },
+    {
+      author: "Oishik Majumdars",
+      title: "Dhusar Jagat",
+      img: "https://m.media-amazon.com/images/I/419UfMbI55L._SX319_BO1,204,203,200_.jpg",
+      id: 2,
+    },
+    {
+      author: "Will Smith",
+      title: "Will",
+      img: "https://m.media-amazon.com/images/I/51povAtTE5L._SX327_BO1,204,203,200_.jpg",
+      id: 3,
+    },
+  ];
+  
--- By john Smilga/booklist/src/index.js
+++ By john Smilga/booklist/src/index.js
@@ -2,49 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
-
-const books = [
-  {
-    author: "Alex Michaelides",
-    title: "THE SILENT PATIENT",
-    img: "https://m.media-amazon.com/images/I/51oVTRsjcqL._SX329_BO1,204,203,200_.jpg",
-    id: 1,
-  },
-  {
-    author: "Oishik Majumdars",
-    title: "Dhusar Jagat",
-    img: "https://m.media-amazon.com/images/I/419UfMbI55L._SX319_BO1,204,203,200_.jpg",
-    id: 2,
-  },
-  {
-    author: "Will Smith",
-    title: "Will",
-    img: "https://m.media-amazon.com/images/I/51povAtTE5L._SX327_BO1,204,203,200_.jpg",
-    id: 3,
-  },
-];
+import {books} from './books'
 
 const BookList = () => {
-  const getBook = (id) => {
-    const book = books.find((book) => book.id === id);
-    console.log(book);
-  };
   return (
     <section className="booklist">
       {books.map((book) => {
-        return <Book {...book} key={book.id} getBook={getBook}/>;
+        return <Book {...book} key={book.id} />;
       })}
     </section>
   );
 };
 
 const Book = (props) => {
-  const { img, title, author, getBook, id } = props;
+  const { img, title, author,  } = props;
   return (
     <article className="book">
       <img src={img} alt={title}></img>
       <h2>{title}</h2>
-      <button onClick={()=>getBook(id)}>Click Me</button>
       <h4>{author}</h4>
     </article>
   );
