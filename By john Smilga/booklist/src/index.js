df734a732c391d9028f58ae434a554486b2426af
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-01-31 22:23:21 +0530

    Booklist project splitting title

--- By john Smilga/booklist/src/index.js
+++ By john Smilga/booklist/src/index.js
@@ -7,41 +7,16 @@ import Book from "./Book";
 
 const BookList = () => {
   return (
+    <>
+    <h1>amazon best selling</h1>
     <section className="booklist">
-      {books.map((book) => {
-        return <Book {...book} key={book.id} />;
+      {books.map((book,index) => {
+        return <Book {...book} key={book.id} number={index} />;
       })}
     </section>
+    </>
   );
 };
-
-
-
-// const EventExamples = () => {
-
-//   const handleButtonClick = () => {
-//     alert("handle button click");
-//   };
-//   const handleFormSubmission = (e) => {
-//     e.preventDefault();
-//     console.log("Form Submitted");
-//   };
-
-//   return (
-//     <section>
-//       <form >
-//         <h2>Typical form</h2>
-//         <input
-//           type="text"
-//           name="example"
-//           onChange={(e=> console.log(e.target.value) )}
-//           style={{ margin: "1rem 0" }}
-//          />
-//       </form>
-//       <button onClick={()=> console.log("click me")}>Click Me</button>
-//     </section>
-//   );
-// };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<BookList />);
