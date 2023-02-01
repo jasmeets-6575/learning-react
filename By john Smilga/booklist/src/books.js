3a9bc017286eda31cf60377118e15ca3fb8a7746
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-02-01 22:23:21 +0530

    Booklist project splitting images

--- By john Smilga/booklist/src/books.js
+++ By john Smilga/booklist/src/books.js
@@ -1,21 +1,24 @@
+import img1 from "./images/book-1.jpg";
+import img2 from "./images/book-2.jpg";
+import img3 from "./images/book-3.jpg";
+
 export const books = [
-    {
-      author: "Alex Michaelides",
-      title: "THE SILENT PATIENT",
-      img: "https://m.media-amazon.com/images/I/51oVTRsjcqL._SX329_BO1,204,203,200_.jpg",
-      id: 1,
-    },
-    {
-      author: "Oishik Majumdars",
-      title: "Dhusar Jagat",
-      img: "https://m.media-amazon.com/images/I/419UfMbI55L._SX319_BO1,204,203,200_.jpg",
-      id: 2,
-    },
-    {
-      author: "Will Smith",
-      title: "Will",
-      img: "https://m.media-amazon.com/images/I/51povAtTE5L._SX327_BO1,204,203,200_.jpg",
-      id: 3,
-    },
-  ];
-  
+  {
+    author: "Alex Michaelides",
+    title: "THE SILENT PATIENT",
+    img: img1,
+    id: 1,
+  },
+  {
+    author: "Oishik Majumdars",
+    title: "Dhusar Jagat",
+    img: img2, 
+    id: 2,
+  },
+  {
+    author: "Will Smith",
+    title: "Will",
+    img: img3,
+    id: 3,
+  },
+];
