019c0726ed6ced6c733f714c6e4eab72b117b5d0
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-02-07 22:23:21 +0530

    BackRoad adding component done

--- By john Smilga/backRoad/src/App.js
+++ By john Smilga/backRoad/src/App.js
@@ -1,8 +1,19 @@
+import { Navbar } from "./components/Navbar";
+import { Hero } from "./components/Hero";
+import { About } from "./components/About";
+import { Services } from "./components/Services";
+import { Tours } from "./components/Tours";
+import { Footer } from "./components/Footer";
+
 function App() {
   return (
-    <>
-
-      
+    <>   
+    <Navbar/>
+    <Hero/>
+    <About/>
+    <Services/>
+    <Tours/>
+    <Footer/>
     </>
   );
 }
