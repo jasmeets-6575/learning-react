04647c1a0b836e223bb5f05b19a5e339d94aff4d
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-02-09 22:23:21 +0530

    BackRoad adding data dynamic

--- By john Smilga/backRoad/src/components/Navbar.js
+++ By john Smilga/backRoad/src/components/Navbar.js
@@ -29,7 +29,10 @@ export const Navbar = () => {
             const {id,href,icon} = link;
             return (
               <li key={id}>
-                <a href={href} target="_blank" className="nav-icon">
+                <a href={href} 
+                target="_blank"
+                rel='noreferrer' 
+                className="nav-icon">
                   <i className={icon}></i>
                 </a>
               </li>
