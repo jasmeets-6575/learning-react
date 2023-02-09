0870897df0bf3011b0c8be3c29c2e93179812a7c
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-02-09 22:23:21 +0530

    BackRoad adding data dynamic

--- By john Smilga/backRoad/src/components/Navbar.js
+++ By john Smilga/backRoad/src/components/Navbar.js
@@ -1,57 +1,43 @@
-import logo from "../images/logo.svg"
-import { pageLinks } from "../data"
+import logo from "../images/logo.svg";
+import { pageLinks, socialLinks } from "../data";
 export const Navbar = () => {
   return (
     <nav className="navbar">
-        <div className="nav-center">
-          <div className="nav-header">
-            <img src={logo} className="nav-logo" alt="backroads" />
-            <button type="button" className="nav-toggle" id="nav-toggle">
-              <i className="fas fa-bars"></i>
-            </button>
-          </div>
-
-          <ul className="nav-links" id="nav-links">
-            {pageLinks.map((link)=>{
-              return (<li key={link.id}>
-              <a href={link.href} className="nav-link">
-                {link.text} 
-              </a>
-            </li>
-            );
-            })}
-          </ul>
-
-          <ul className="nav-icons">
-            <li>
-              <a
-                href="https://www.twitter.com"
-                target="_blank"
-                className="nav-icon"
-              >
-                <i className="fab fa-facebook"></i>
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.twitter.com"
-                target="_blank"
-                className="nav-icon"
-              >
-                <i className="fab fa-twitter"></i>
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.twitter.com"
-                target="_blank"
-                className="nav-icon"
-              >
-                <i className="fab fa-squarespace"></i>
-              </a>
-            </li>
-          </ul>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="nav-logo" alt="backroads" />
+          <button type="button" className="nav-toggle" id="nav-toggle">
+            <i className="fas fa-bars"></i>
+          </button>
         </div>
-      </nav>
-  )
-}
+
+        <ul className="nav-links" id="nav-links">
+          {pageLinks.map((link) => {
+            const {id,href,text} = link;
+            return (
+              <li key={id}>
+                <a href={href} className="nav-link">
+                  {text}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+
+        <ul className="nav-icons">
+          {socialLinks.map((link) => {
+            const {id,href,icon} = link;
+            return (
+              <li key={id}>
+                <a href={href} target="_blank" className="nav-icon">
+                  <i className={icon}></i>
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+
+      </div>
+    </nav>
+  );
+};
