b4c7a8a2d51263cf8c08d4716bf355494373b2e1
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-02-10 22:23:21 +0530

    BackRoad adding title

--- By john Smilga/backRoad/src/components/Services.js
+++ By john Smilga/backRoad/src/components/Services.js
@@ -1,50 +1,50 @@
-import Title from "./Title"
+import Title from "./Title";
 const Services = () => {
   return (
     <section className="section services" id="services">
-       <Title title="our" subTitle="services"/>
+      <Title title="our" subTitle="services" />
 
-        <div className="section-center services-center">
-          <article className="service">
-            <span className="service-icon">
-              <i className="fas fa-wallet fa-fw"></i>
-            </span>
-            <div className="service-info">
-              <h4 className="service-title">saving money</h4>
-              <p className="service-text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Asperiores, officia.
-              </p>
-            </div>
-          </article>
+      <div className="section-center services-center">
+        <article className="service">
+          <span className="service-icon">
+            <i className="fas fa-wallet fa-fw"></i>
+          </span>
+          <div className="service-info">
+            <h4 className="service-title">saving money</h4>
+            <p className="service-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Asperiores, officia.
+            </p>
+          </div>
+        </article>
 
-          <article className="service">
-            <span className="service-icon">
-              <i className="fas fa-tree fa-fw"></i>
-            </span>
-            <div className="service-info">
-              <h4 className="service-title">endless hiking</h4>
-              <p className="service-text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Asperiores, officia.
-              </p>
-            </div>
-          </article>
+        <article className="service">
+          <span className="service-icon">
+            <i className="fas fa-tree fa-fw"></i>
+          </span>
+          <div className="service-info">
+            <h4 className="service-title">endless hiking</h4>
+            <p className="service-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Asperiores, officia.
+            </p>
+          </div>
+        </article>
 
-          <article className="service">
-            <span className="service-icon">
-              <i className="fas fa-socks fa-fw"></i>
-            </span>
-            <div className="service-info">
-              <h4 className="service-title">amazing comfort</h4>
-              <p className="service-text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Asperiores, officia.
-              </p>
-            </div>
-          </article>
-        </div>
-      </section>
-  )
-}
-export default Services ;
+        <article className="service">
+          <span className="service-icon">
+            <i className="fas fa-socks fa-fw"></i>
+          </span>
+          <div className="service-info">
+            <h4 className="service-title">amazing comfort</h4>
+            <p className="service-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Asperiores, officia.
+            </p>
+          </div>
+        </article>
+      </div>
+    </section>
+  );
+};
+export default Services;
--- By john Smilga/backRoad/src/components/Tours.js
+++ By john Smilga/backRoad/src/components/Tours.js
@@ -1,115 +1,114 @@
-import Title from "./Title"
+import Title from "./Title";
 const Tours = () => {
   return (
     <section className="section" id="tours">
-    
-    <Title title="featured" subTitle="tours"/>
+      <Title title="featured" subTitle="tours" />
 
-        <div className="section-center featured-center">
-          <article className="tour-card">
-            <div className="tour-img-container">
-              <img src="./images/tour-1.jpeg" className="tour-img" alt="" />
-              <p className="tour-date">august 26th, 2020</p>
+      <div className="section-center featured-center">
+        <article className="tour-card">
+          <div className="tour-img-container">
+            <img src="./images/tour-1.jpeg" className="tour-img" alt="" />
+            <p className="tour-date">august 26th, 2020</p>
+          </div>
+          <div className="tour-info">
+            <div className="tour-title">
+              <h4>Tibet Adventure</h4>
             </div>
-            <div className="tour-info">
-              <div className="tour-title">
-                <h4>Tibet Adventure</h4>
-              </div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque
+              vitae tempore voluptatum maxime reprehenderit eum quod
+              exercitationem fugit, qui corporis.
+            </p>
+            <div className="tour-footer">
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque
-                vitae tempore voluptatum maxime reprehenderit eum quod
-                exercitationem fugit, qui corporis.
+                <span>
+                  <i className="fas fa-map"></i>
+                </span>{" "}
+                china
               </p>
-              <div className="tour-footer">
-                <p>
-                  <span>
-                    <i className="fas fa-map"></i>
-                  </span>{" "}
-                  china
-                </p>
-                <p>6 days</p>
-                <p>from $2100</p>
-              </div>
+              <p>6 days</p>
+              <p>from $2100</p>
             </div>
-          </article>
+          </div>
+        </article>
 
-          <article className="tour-card">
-            <div className="tour-img-container">
-              <img src="./images/tour-2.jpeg" className="tour-img" alt="" />
-              <p className="tour-date">october 1th, 2020</p>
-            </div>
-            <div className="tour-info">
-              <h4>best of java</h4>
+        <article className="tour-card">
+          <div className="tour-img-container">
+            <img src="./images/tour-2.jpeg" className="tour-img" alt="" />
+            <p className="tour-date">october 1th, 2020</p>
+          </div>
+          <div className="tour-info">
+            <h4>best of java</h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque
+              vitae tempore voluptatum maxime reprehenderit eum quod
+              exercitationem fugit, qui corporis.
+            </p>
+            <div className="tour-footer">
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque
-                vitae tempore voluptatum maxime reprehenderit eum quod
-                exercitationem fugit, qui corporis.
+                <span>
+                  <i className="fas fa-map"></i>
+                </span>{" "}
+                indonesia
               </p>
-              <div className="tour-footer">
-                <p>
-                  <span>
-                    <i className="fas fa-map"></i>
-                  </span>{" "}
-                  indonesia
-                </p>
-                <p>11 days</p>
-                <p>from $1400</p>
-              </div>
+              <p>11 days</p>
+              <p>from $1400</p>
             </div>
-          </article>
+          </div>
+        </article>
 
-          <article className="tour-card">
-            <div className="tour-img-container">
-              <img src="./images/tour-3.jpeg" className="tour-img" alt="" />
-              <p className="tour-date">september 15th, 2020</p>
-            </div>
-            <div className="tour-info">
-              <h4>explore hong kong</h4>
+        <article className="tour-card">
+          <div className="tour-img-container">
+            <img src="./images/tour-3.jpeg" className="tour-img" alt="" />
+            <p className="tour-date">september 15th, 2020</p>
+          </div>
+          <div className="tour-info">
+            <h4>explore hong kong</h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque
+              vitae tempore voluptatum maxime reprehenderit eum quod
+              exercitationem fugit, qui corporis.
+            </p>
+            <div className="tour-footer">
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque
-                vitae tempore voluptatum maxime reprehenderit eum quod
-                exercitationem fugit, qui corporis.
+                <span>
+                  <i className="fas fa-map"></i>
+                </span>{" "}
+                hong kong
               </p>
-              <div className="tour-footer">
-                <p>
-                  <span>
-                    <i className="fas fa-map"></i>
-                  </span>{" "}
-                  hong kong
-                </p>
-                <p>8 days</p>
-                <p>from $5000</p>
-              </div>
+              <p>8 days</p>
+              <p>from $5000</p>
             </div>
-          </article>
+          </div>
+        </article>
 
-          <article className="tour-card">
-            <div className="tour-img-container">
-              <img src="./images/tour-4.jpeg" className="tour-img" alt="" />
-              <p className="tour-date">december 5th, 2019</p>
-            </div>
-            <div className="tour-info">
-              <h4>kenya highlights</h4>
+        <article className="tour-card">
+          <div className="tour-img-container">
+            <img src="./images/tour-4.jpeg" className="tour-img" alt="" />
+            <p className="tour-date">december 5th, 2019</p>
+          </div>
+          <div className="tour-info">
+            <h4>kenya highlights</h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque
+              vitae tempore voluptatum maxime reprehenderit eum quod
+              exercitationem fugit, qui corporis.
+            </p>
+            <div className="tour-footer">
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque
-                vitae tempore voluptatum maxime reprehenderit eum quod
-                exercitationem fugit, qui corporis.
+                <span>
+                  <i className="fas fa-map"></i>
+                </span>{" "}
+                kenya
               </p>
-              <div className="tour-footer">
-                <p>
-                  <span>
-                    <i className="fas fa-map"></i>
-                  </span>{" "}
-                  kenya
-                </p>
-                <p>20 days</p>
-                <p>from $3300</p>
-              </div>
+              <p>20 days</p>
+              <p>from $3300</p>
             </div>
-          </article>
-        </div>
-      </section>
-  )
-}
+          </div>
+        </article>
+      </div>
+    </section>
+  );
+};
 
-export default Tours ;
+export default Tours;
