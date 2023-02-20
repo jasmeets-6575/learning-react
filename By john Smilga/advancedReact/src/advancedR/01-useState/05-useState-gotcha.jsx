ec9eb9fbdb6064d3e4832dd2729cd1f063eff2f7
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-02-20 23:23:21 +0530

    use state gotcha

--- By john Smilga/advancedReact/src/advancedR/01-useState/05-useState-gotcha.jsx
+++ By john Smilga/advancedReact/src/advancedR/01-useState/05-useState-gotcha.jsx
@@ -3,11 +3,13 @@ import { useState } from "react";
 const UseStateGotcha = () => {
   const [value, setValue] = useState(0);
   const handleClick = () => {
-    setValue((currentState)=>{
-        const newState =currentState +1;
-        console.log(newState);
-        return newState;
-    });
+    setTimeout(()=>{
+        setValue((currentState)=>{
+            const newState =currentState +1;
+            console.log(newState);
+            return newState;
+        })
+    },2000)
   };
   return (
     <div>
