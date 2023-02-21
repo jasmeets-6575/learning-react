3aae9120d23aae7e32898e678b333c388fc206fa
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-02-21 23:23:21 +0530

    birthday project

--- By john Smilga/projects/01-birthdayProject/my-react-app/src/List.jsx
+++ By john Smilga/projects/01-birthdayProject/my-react-app/src/List.jsx
@@ -1,22 +1,22 @@
 import data from "./data";
 
-const List = ({people}) => {
-  return <>
-    
-    {people.map((person) => {
-      const { id, name, image, age } = person;
-      return (
-            <article key={id} className="person">
+const List = ({ people }) => {
+  return (
+    <>
+      {people.map((person) => {
+        const { id, name, image, age } = person;
+        return (
+          <article key={id} className="person">
             <img src={image} alt={name} />
             <div>
-            <h4>{name}</h4>
-            <p>{age}</p>
+              <h4>{name}</h4>
+              <p>{age}</p>
             </div>
           </article>
-      );
-    }
-  )}
-  </>;
+        );
+      })}
+    </>
+  );
 };
 
 export default List;
