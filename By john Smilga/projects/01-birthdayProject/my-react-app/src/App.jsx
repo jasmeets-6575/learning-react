90281ae82459e7d6db1017849b111352347965c3
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-02-21 23:23:21 +0530

    birthday project

--- By john Smilga/projects/01-birthdayProject/my-react-app/src/App.jsx
+++ By john Smilga/projects/01-birthdayProject/my-react-app/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <section className='container'>
       <h3>{people.length} Birthdays Today</h3>
       <List people={people} />
-      <button> Clear All</button>
+      <button onClick={()=>setPeople([])}> Clear All</button>
     </section>
   </main>
 }
