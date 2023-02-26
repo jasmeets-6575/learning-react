ed144a7bfe507e5a17e57b536e930451a27942e7
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-02-26 23:23:21 +0530

    UseEffect fetch data

--- By john Smilga/advancedReact/src/advancedR/02-useEffect/04-fetch-data.jsx
+++ By john Smilga/advancedReact/src/advancedR/02-useEffect/04-fetch-data.jsx
@@ -4,14 +4,12 @@ const url = "https://api.github.com/users";
 
 const FetchData = () => {
   const [users, setUsers] = useState([]);
-
   useEffect(() => {
     const fetchData = async () => {
     try {
         const response = await fetch(url);
-        const users = await response.json();
-       console.log(users);
-      
+        const users = await response.json(); 
+        setUsers(users);     
     } catch (error) {
       console.log(error);
     }
@@ -20,9 +18,22 @@ const FetchData = () => {
   }, []);
 
   return (
-    <div>
-      <h2>fetch data</h2>
-    </div>
+    <section>
+      <h3>Github Users</h3>
+      <ul className="users">
+        {users.map((user)=>{
+          const {id, login,avatar_url,html_url}=user
+         return  <li key={id}>
+          <img src={avatar_url} alt={login} style={{width:"100px"}} />
+          <div>
+            <h5>{login}</h5>
+            <a href={html_url}>profile</a>
+          </div>
+         </li>
+
+        })}
+      </ul>
+    </section>
   );
 };
 
