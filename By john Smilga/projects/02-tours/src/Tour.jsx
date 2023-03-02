fb6bc1762491078e186e0fa4ee68b870a11ed7f5
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-03-02 23:23:21 +0530

    tour project

--- By john Smilga/projects/02-tours/src/Tour.jsx
+++ By john Smilga/projects/02-tours/src/Tour.jsx
@@ -1,10 +1,18 @@
 import React, {useState} from 'react'
 
-const Tour = () => {
+const Tour = ({id,image,info,price,name}) => {
   return (
-    <div>
-      <h2>single tour component</h2>
-    </div>
+    <article className='single-tour'>
+      <img src={image} alt={name} />
+      <footer>
+        <div className='tour-info'>
+          <h4>{name}</h4>
+          <h4 className='tour-price'>${price}</h4>
+        </div>
+        <p>{info}</p>
+        <button className="delete-btn">not interested</button>
+      </footer>
+    </article>
   )
 }
 
