26afc8b268db56d44f486e2fa61175a12c19a23c
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-03-02 23:23:21 +0530

    tour project remove btn

--- By john Smilga/projects/02-tours/src/App.jsx
+++ By john Smilga/projects/02-tours/src/App.jsx
@@ -9,6 +9,10 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
 
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour)=>tour.id !==id);
+    setTours(newTours);
+  }
   const fetchTours = async () => {
     setLoading(true);
     
@@ -36,7 +40,7 @@ function App() {
   }
   return (
     <main>
-      <Tours tours={tours} />
+      <Tours tours={tours} removeTour={removeTour} />
     </main>
   );
 }
