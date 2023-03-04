661a82463ecbbccdf72eb33fa435ea8c51f5ad20
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-03-04 23:23:21 +0530

    toggle cleanupe

--- By john Smilga/advancedReact/src/advancedR/03-conditonal-rendering/07-cleanup-function.jsx
+++ By john Smilga/advancedReact/src/advancedR/03-conditonal-rendering/07-cleanup-function.jsx
@@ -1,12 +1,21 @@
-import {useState , useEffect} from 'react'
+import { useState, useEffect } from "react";
 
 const CleanupFunction = () => {
-  const [toggle , setToggle ] = useState(false)
+  const [toggle, setToggle] = useState(false);
 
+  return (
+    <div>
+      <button className="btn" onClick={() => setToggle(!toggle)}>
+        toggle component
+      </button>
+      {toggle && <RandomComponent/>}
+    </div>
+  );
+};
 
-  return ( <div>
-    <button className='btn' onClick={()=>setToggle(!toggle)}>toggle component</button>
-  </div> )
-}
+const RandomComponent = () => {
+    useEffect(()=>{console.log("hmm... this is interesting");},[])
+    return <h2>hello there</h2>
+};
 
-export default CleanupFunction
+export default CleanupFunction;
