beccf61519177b8bd047fa40a480737d8c100b75
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-03-07 20:23:09 +0530

    intro

---
+++ By john Smilga/my-app/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function Greeting(){
+    return <h2>My First Component</h2>
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<Greeting></Greeting>);
+
