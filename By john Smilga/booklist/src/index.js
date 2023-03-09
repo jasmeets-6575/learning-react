13bebd39e44f528da6e976c9bb93f3992fe019e9
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-03-09 16:28:02 +0530

    Booklist project adding key prop

--- By john Smilga/booklist/src/index.js
+++ By john Smilga/booklist/src/index.js
@@ -8,16 +8,19 @@ const books = [
     author: "Alex Michaelides",
     title: "THE SILENT PATIENT",
     img: "https://m.media-amazon.com/images/I/51oVTRsjcqL._SX329_BO1,204,203,200_.jpg",
+    id:1,
   },
   {
     author: "Oishik Majumdars",
     title: "Dhusar Jagat",
     img: "https://m.media-amazon.com/images/I/419UfMbI55L._SX319_BO1,204,203,200_.jpg",
+    id:2,
   },
   {
     author: "Will Smith",
     title: "Will",
     img: "https://m.media-amazon.com/images/I/51povAtTE5L._SX327_BO1,204,203,200_.jpg",
+    id:3,
   },
 ];
 
@@ -25,8 +28,8 @@ function Booklist() {
   return (
     <section className="booklist">
       {books.map((book) => {
-        const { img, title, author } = book;
-        return <Book img={img} title={title} author={author} />;
+        const { img, title, author,id } = book;
+        return <Book img={img} title={title} author={author} key={id} />;
       })}
     </section>
   );
