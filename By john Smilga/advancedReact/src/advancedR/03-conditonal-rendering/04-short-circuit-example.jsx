11b9ca37d90e7e03707002192c5d7d74d8d33bed
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-03-14 19:53:28 +0530

    short circuit eg

--- By john Smilga/advancedReact/src/advancedR/03-conditonal-rendering/04-short-circuit-example.jsx
+++ By john Smilga/advancedReact/src/advancedR/03-conditonal-rendering/04-short-circuit-example.jsx
@@ -1,34 +1,46 @@
-import {useState} from 'react'
+import { useState } from "react";
 
 const ShortCircuitExample = () => {
-  const [text,setText] = useState('');
-  const [name,setName] = useState('Susan');
-  const [user,setUser] = useState({name:"john"});
-  const [isEditing,setIsEditing] = useState(false);
-    return (
+  const [text, setText] = useState("");
+  const [name, setName] = useState("Susan");
+  const [user, setUser] = useState({ name: "john" });
+  const [isEditing, setIsEditing] = useState(false);
+  return (
+    <div>
+      <h2>{text || "default value"}</h2>
+      {text && (
         <div>
-            <h2>{text || "default value" }</h2>
-            {text && (
-                <div>
-                    <h2>whatever return</h2>
-                    <h2>{name}</h2>
-                </div>
-            )}
-            {/* {!text && (
+          <h2>whatever return</h2>
+          <h2>{name}</h2>
+        </div>
+      )}
+      {/* {!text && (
                 <div>
                     <h2>whatever return</h2>
                     <h2>{name}</h2>
                 </div>
             )} */}
-            {user && <SomeComponent name ={user.name} />}
-        </div>
-    )
-}
-const SomeComponent = ({name}) => {
-    return <div>
-    <h2>whatever return</h2>
-    <h2>{name}</h2>
-</div>
-}
+      {user && <SomeComponent name={user.name} />}
+      <h2 style={{margin:"1rem 0"}}>Ternary Operartor</h2>
+      <button className="btn">{isEditing ? "edit":"add"}</button>
+      {user ? (
+        <div>
+            <h4> hello there user {user.name}</h4>
+        </div>) : (
+            <div>
+                <h2>hello there {user.name} you need to login</h2>
+            </div>
+      )}
+    </div>
+  );
+};
+const SomeComponent = ({ name }) => {
+  return (
+    <div>
+      <h2>whatever return</h2>
+      <h2>{name}</h2>
+    </div>
+  );
+};
 
-export default ShortCircuitExample
+export default ShortCircuitExample;
