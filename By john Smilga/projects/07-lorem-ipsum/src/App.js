acc0f6bc869869417bb8c92c24b78421ee86393e
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-03-22 14:53:33 +0530

    Lorem ipsum project

--- By john Smilga/projects/07-lorem-ipsum/src/App.js
+++ By john Smilga/projects/07-lorem-ipsum/src/App.js
@@ -1,27 +1,46 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from "react";
+import data from "./data";
 
 function App() {
-  const [count,setCount]=useState(0);
-  const[text,setText]=useState([])
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
 
-  const handleSubmit = (e) =>{
+  const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("hello world");
-  }
+    let amount = parseInt(count);
 
-  return (<section className='section-center'>
-    <h3>Tired of boring lorem ipsum ?</h3>
-    <form action="lorem-form" onSubmit={{handleSubmit}}>
-      <label htmlFor="amount" >paragraphs :</label>  
-      <input type="number" name="amount" id="amount" value={count} onChange={(e)=>setCount(e.target.value)} />
-      <button type="submit" className='btn'>generate </button>
-    </form>  
-    <article className='lorem-text'>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo est delectus labore facilis placeat fuga ullam sit, magni quibusdam ipsa</p>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo est delectus labore facilis placeat fuga ullam sit, magni quibusdam ipsa</p>
-    </article>
-    </section>)
+    if (amount <= 0) {
+      amount = 1;
+    }
+    if (count > 8) {
+      amount = 8;
+    }
+    setText(data.slice(0, amount));
+  };
+
+  return (
+    <section className="section-center">
+      <h3>Tired of boring lorem ipsum ?</h3>
+      <form action="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraphs :</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit" className="btn">
+          generate{" "}
+        </button>
+      </form>
+      <article className="lorem-text">
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
+      </article>
+    </section>
+  );
 }
 
 export default App;
