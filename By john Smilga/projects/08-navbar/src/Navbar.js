5977a1b5d0a997f73d4457cce0dbd1722a1394e2
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-03-23 23:23:12 +0530

    Navbar project

--- By john Smilga/projects/08-navbar/src/Navbar.js
+++ By john Smilga/projects/08-navbar/src/Navbar.js
@@ -1,44 +1,41 @@
-import React, { useState, useRef, useEffect } from 'react'
-import { FaBars, FaTwitter } from 'react-icons/fa'
-import { links, social } from './data'
-import logo from './logo.svg'
+import React, { useState, useRef, useEffect } from "react";
+import { FaBars, FaTwitter } from "react-icons/fa";
+import { links, social } from "./data";
+import logo from "./logo.svg";
 
 const Navbar = () => {
-  return <nav>
-    <div className='nav-center'>
-      <div className='nav-header'>
-        <img src={logo} alt="logo" />
-        <button className='nav-toggle'><FaBars/></button>
-      </div>
-      <div className='links-container show-container'>
-        <ul className='links'>
-          <li>
-            <a href='#'>home</a>
-          </li>
-          <li>
-            <a href='#'>about</a>
-          </li>
-          <li>
-            <a href='#'>contact</a>
-          </li>
-          <li>
-            <a href='#'>product</a>
-          </li>
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} alt="logo" />
+          <button className="nav-toggle">
+            <FaBars />
+          </button>
+        </div>
+        <div className="links-container show-container">
+          <ul className="links">
+            {links.map((link) => {
+              const { id, url, text } = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <ul className="social-icons">
+          {social.map((links)=>{
+            const {id,url,icon} = links
+            return (<li key={id}>
+              <a href={url}>{icon}</a>
+            </li>)
+          })}
         </ul>
       </div>
-      <ul className='social-icons'>
-        <li>
-          <a href='#'><FaTwitter/></a>
-        </li>
-        <li>
-          <a href='#'><FaTwitter/></a>
-        </li>
-        <li>
-          <a href='#'><FaTwitter/></a>
-        </li>
-      </ul>
-    </div>
-  </nav>
-}
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
