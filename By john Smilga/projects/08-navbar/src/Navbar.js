84bfbe6f7c9b186102399a278d880864ff2e9b78
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-03-23 23:23:12 +0530

    Navbar project

--- By john Smilga/projects/08-navbar/src/Navbar.js
+++ By john Smilga/projects/08-navbar/src/Navbar.js
@@ -4,7 +4,41 @@ import { links, social } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
-  return <h4>navbar</h4>
+  return <nav>
+    <div className='nav-center'>
+      <div className='nav-header'>
+        <img src={logo} alt="logo" />
+        <button className='nav-toggle'><FaBars/></button>
+      </div>
+      <div className='links-container show-container'>
+        <ul className='links'>
+          <li>
+            <a href='#'>home</a>
+          </li>
+          <li>
+            <a href='#'>about</a>
+          </li>
+          <li>
+            <a href='#'>contact</a>
+          </li>
+          <li>
+            <a href='#'>product</a>
+          </li>
+        </ul>
+      </div>
+      <ul className='social-icons'>
+        <li>
+          <a href='#'><FaTwitter/></a>
+        </li>
+        <li>
+          <a href='#'><FaTwitter/></a>
+        </li>
+        <li>
+          <a href='#'><FaTwitter/></a>
+        </li>
+      </ul>
+    </div>
+  </nav>
 }
 
 export default Navbar
