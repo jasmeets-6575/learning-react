eb4c939e0539332e9b05f983d2b6aefa30da2433
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-03-23 23:23:12 +0530

    Navbar project

--- By john Smilga/projects/08-navbar/src/Navbar.js
+++ By john Smilga/projects/08-navbar/src/Navbar.js
@@ -4,15 +4,17 @@ import { links, social } from "./data";
 import logo from "./logo.svg";
 
 const Navbar = () => {
+  const [showLinks,setShowLinks] = useState(false)
   return (
     <nav>
       <div className="nav-center">
         <div className="nav-header">
           <img src={logo} alt="logo" />
-          <button className="nav-toggle">
+          <button className="nav-toggle" onClick={()=>setShowLinks(!showLinks)}>
             <FaBars />
           </button>
         </div>
+        {showLinks && (
         <div className="links-container show-container">
           <ul className="links">
             {links.map((link) => {
@@ -25,6 +27,7 @@ const Navbar = () => {
             })}
           </ul>
         </div>
+        )}
         <ul className="social-icons">
           {social.map((links)=>{
             const {id,url,icon} = links
