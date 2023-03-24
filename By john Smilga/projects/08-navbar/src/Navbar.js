922a7269f1473197350af76c308183c07f327b3e
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-03-24 23:23:12 +0530

    Navbar project

--- By john Smilga/projects/08-navbar/src/Navbar.js
+++ By john Smilga/projects/08-navbar/src/Navbar.js
@@ -4,19 +4,34 @@ import { links, social } from "./data";
 import logo from "./logo.svg";
 
 const Navbar = () => {
-  const [showLinks,setShowLinks] = useState(false)
+  const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = "0px";
+    }
+  }, [showLinks]);
+
   return (
     <nav>
       <div className="nav-center">
         <div className="nav-header">
           <img src={logo} alt="logo" />
-          <button className="nav-toggle" onClick={()=>setShowLinks(!showLinks)}>
+          <button
+            className="nav-toggle"
+            onClick={() => setShowLinks(!showLinks)}
+          >
             <FaBars />
           </button>
         </div>
-        {showLinks && (
-        <div className="links-container show-container">
-          <ul className="links">
+
+        <div className="links-container" ref={linksContainerRef}>
+          <ul className="links" ref={linksRef}>
             {links.map((link) => {
               const { id, url, text } = link;
               return (
@@ -27,13 +42,15 @@ const Navbar = () => {
             })}
           </ul>
         </div>
-        )}
+
         <ul className="social-icons">
-          {social.map((links)=>{
-            const {id,url,icon} = links
-            return (<li key={id}>
-              <a href={url}>{icon}</a>
-            </li>)
+          {social.map((links) => {
+            const { id, url, icon } = links;
+            return (
+              <li key={id}>
+                <a href={url}>{icon}</a>
+              </li>
+            );
           })}
         </ul>
       </div>
