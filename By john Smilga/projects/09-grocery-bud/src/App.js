8bc1f66d983e49abdab433034bc7f2902d4690fa
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-03-25 23:23:12 +0530

    Grocery Bud

---
+++ By john Smilga/projects/09-grocery-bud/src/App.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import List from "./List";
+import Alert from "./Alert";
+
+function App() {
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("hello");
+  };
+
+  return (
+    <section className="section-center">
+      <form className="grocery-form" onSubmit={handleSubmit}></form>
+      {alert.show && <Alert />}
+      <h3>grocery bud</h3>
+      <div className="form-control">
+        <input
+          type="text"
+          className="grocery"
+          placeholder="e.g. eggs"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button type="submit" className="submit-btn">
+          {isEditing ? "edit" : "submit"}
+        </button>
+      </div>
+      <div className="grocery-container">
+        <List />
+        <button className="clear-btn">clear items</button>
+      </div>
+    </section>
+  );
+}
+
+export default App;
