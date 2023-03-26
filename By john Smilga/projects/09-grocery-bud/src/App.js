34217440b1173e3537f6f8e80221d1fe754bd13c
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-03-26 19:35:23 +0530

    Grocery Bud

--- By john Smilga/projects/09-grocery-bud/src/App.js
+++ By john Smilga/projects/09-grocery-bud/src/App.js
@@ -11,43 +11,47 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if(!name){
+    if (!name) {
       // display alert
-    } else if (name && isEditing){
+      showAlert(true, "danger", "please enter value");
+    } else if (name && isEditing) {
       // deal with edit
-    }
-    else{
+    } else {
       // show alert
-      const newItem = {id: new Date().getTime().toString(),title:name};
-      setList([...list,newItem])
-      setName('');
+      const newItem = { id: new Date().getTime().toString(), title: name };
+      setList([...list, newItem]);
+      setName("");
     }
+  };
+
+  const showAlert = (show = false, type = "", msg = "") => {
+    setAlert({ show, type, msg });
   };
 
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
-      {alert.show && <Alert />}
-      <h3>grocery bud</h3>
-      <div className="form-control">
-        <input
-          type="text"
-          className="grocery"
-          placeholder="e.g. eggs"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <button type="submit" className="submit-btn">
-          {isEditing ? "edit" : "submit"}
-        </button>     
-      </div>
+        {alert.show && <Alert {...alert} removeAlert={showAlert} />}
+        <h3>grocery bud</h3>
+        <div className="form-control">
+          <input
+            type="text"
+            className="grocery"
+            placeholder="e.g. eggs"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type="submit" className="submit-btn">
+            {isEditing ? "edit" : "submit"}
+          </button>
+        </div>
       </form>
 
       {list.length > 0 && (
-      <div className="grocery-container">
-        <List items={list} />
-        <button className="clear-btn">clear items</button>
-      </div>
+        <div className="grocery-container">
+          <List items={list} />
+          <button className="clear-btn">clear items</button>
+        </div>
       )}
     </section>
   );
