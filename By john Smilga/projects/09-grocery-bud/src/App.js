a587703ecd7f88a2888ff2652f317f4619a25fef
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-03-26 20:41:41 +0530

    Grocery Bud edit

--- By john Smilga/projects/09-grocery-bud/src/App.js
+++ By john Smilga/projects/09-grocery-bud/src/App.js
@@ -15,7 +15,18 @@ function App() {
       // display alert
       showAlert(true, "danger", "please enter value");
     } else if (name && isEditing) {
-      // deal with edit
+      setList(
+        list.map((item)=>{
+          if(item.id===editID){
+            return{...item,title:name}
+          }
+          return item
+        })
+      )
+      setName('');
+      setEditID(null);
+      setIsEditing(false)
+      showAlert(true,'success','value changed')
     } else {
       showAlert(true, "success", "item added to the list");
       const newItem = { id: new Date().getTime().toString(), title: name };
@@ -32,11 +43,18 @@ function App() {
     showAlert(true, "danger", "empty list");
     setList([]);
   };
-  
+
   const removeItem = (id) => {
-    showAlert(true,'danger','item removed from list')
-    setList(list.filter((item)=> item.id !==id))
-  }
+    showAlert(true, "danger", "item removed from list");
+    setList(list.filter((item) => item.id !== id));
+  };
+
+  const editItem = (id) => {
+    const specificItem = list.filter((item) => item.id !== id);
+    setIsEditing(true);
+    setEditID(id)
+    setName(specificItem.title)
+  };
 
   return (
     <section className="section-center">
@@ -59,7 +77,7 @@ function App() {
 
       {list.length > 0 && (
         <div className="grocery-container">
-          <List items={list} removeItem={removeItem} />
+          <List items={list} removeItem={removeItem} editItem={editItem} />
           <button className="clear-btn" onClick={clearList}>
             clear items
           </button>
