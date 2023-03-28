b9c705656e142faaeb5e6b67a13c344f0cf9e713
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-03-28 19:55:10 +0530

    stripe project

--- By john Smilga/projects/10-sidebar-modal/src/Home.js
+++ By john Smilga/projects/10-sidebar-modal/src/Home.js
@@ -1,16 +1,18 @@
-import React, { useContext } from "react";
+import React from "react";
 import { FaBars } from "react-icons/fa";
-import { AppContext ,useGlobalContext } from "./context";
+import { useGlobalContext } from "./context";
 
 const Home = () => {
-  const data = useGlobalContext()
-  
+  const { openSidebar, openModal } = useGlobalContext();
+
   return (
     <main>
-      <button className="sidebar-toggle">
+      <button className="sidebar-toggle" onClick={openSidebar}>
         <FaBars />
       </button>
-      <button className="btn">show modal</button>
+      <button className="btn" onClick={openModal}>
+        show modal
+      </button>
     </main>
   );
 };
--- By john Smilga/projects/10-sidebar-modal/src/context.js
+++ By john Smilga/projects/10-sidebar-modal/src/context.js
@@ -3,12 +3,41 @@ import React, { useState, useContext } from "react";
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value="hello">{children}</AppContext.Provider>;
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        isSidebarOpen,
+        isModalOpen,
+        openSidebar,
+        closeSidebar,
+        openModal,
+        closeModal,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
 };
 
 // custom hook
 export const useGlobalContext = () => {
-    return useContext(AppContext)
-}
+  return useContext(AppContext);
+};
 
 export { AppContext, AppProvider };
