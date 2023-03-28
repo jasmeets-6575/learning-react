3d5a820b4ec8ee8067032f9821b36569554731bf
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-03-28 23:18:23 +0530

    stripe project

--- By john Smilga/projects/11-stripe-submenus/src/Navbar.js
+++ By john Smilga/projects/11-stripe-submenus/src/Navbar.js
@@ -5,6 +5,9 @@ import { FaBars } from "react-icons/fa";
 
 const Navbar = () => {
   const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext();
+  const displaySubmenu = (e) => {
+    openSubmenu()
+  }
   return (
     <nav className="nav">
       <div className="nav-center">
@@ -16,13 +19,13 @@ const Navbar = () => {
         </div>
         <ul className="nav-links">
           <li>
-            <button className="link-btn">products</button>
+            <button className="link-btn" onMouseOver={displaySubmenu}>products</button>
           </li>
           <li>
-            <button className="link-btn">developers</button>
+            <button className="link-btn" onMouseOver={displaySubmenu}>developers</button>
           </li>
           <li>
-            <button className="link-btn">company</button>
+            <button className="link-btn" onMouseOver={displaySubmenu}>company</button>
           </li>
         </ul>
         <button className="btn signin-btn">Sign in</button>
--- By john Smilga/projects/11-stripe-submenus/src/Submenu.js
+++ By john Smilga/projects/11-stripe-submenus/src/Submenu.js
@@ -1,8 +1,13 @@
-import React, { useState, useRef, useEffect } from 'react'
-import { useGlobalContext } from './context'
+import React, { useState, useRef, useEffect } from "react";
+import { useGlobalContext } from "./context";
 
 const Submenu = () => {
-  return <h2>submenu component</h2>
-}
+  const { isSubmenuOpen } = useGlobalContext();
+  return (
+    <aside className={`${isSubmenuOpen ? "submenu show" : "submenu"}`}>
+      submenu component
+    </aside>
+  );
+};
 
-export default Submenu
+export default Submenu;
