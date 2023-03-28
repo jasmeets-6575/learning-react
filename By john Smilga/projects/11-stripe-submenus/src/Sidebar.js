b42442bb7bc9dce9bb3ba4288b8f8c26d126bf3b
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-03-28 23:09:03 +0530

    stripe project

--- By john Smilga/projects/11-stripe-submenus/src/Sidebar.js
+++ By john Smilga/projects/11-stripe-submenus/src/Sidebar.js
@@ -4,7 +4,36 @@ import { useGlobalContext } from './context'
 import sublinks from './data'
 
 const Sidebar = () => {
-  return <h2>sidebar component</h2>
+  const {isSidebarOpen,closeSidebar} = useGlobalContext()
+
+  return <aside className={`${isSidebarOpen ? 'sidebar-wrapper show' : 'sidebar-wrapper'}`}>
+      <div className="sidebar">
+        <button className="close-btn" onClick={closeSidebar}>
+          <FaTimes/>
+        </button>
+        <div className="sidebar-links">
+          {sublinks.map((item,index)=>{
+            const {links,page} = item
+            return (
+              <article key={index}>
+                <h4>{page}</h4>
+                <div className="sidebar-sublinks">
+                   {links.map((link,index)=>{
+                     const {url,icon,label} =link
+                     return (
+                      <a key={index} href={url}>
+                        {icon}
+                        {label}
+                      </a>
+                     )
+                   })}
+                </div>
+              </article>
+            )
+          })}
+        </div>
+      </div>
+    </aside>
 }
 
 export default Sidebar
