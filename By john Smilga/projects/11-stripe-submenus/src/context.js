d3f149798bec11a13bcb489fae36f631ac53f5bd
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-03-28 21:16:23 +0530

    stripe project

--- By john Smilga/projects/11-stripe-submenus/src/context.js
+++ By john Smilga/projects/11-stripe-submenus/src/context.js
@@ -1,2 +1,41 @@
-import React, { useState, useContext } from 'react'
-import sublinks from './data'
+import React, { useState, useContext, createContext } from "react";
+import sublinks from "./data";
+
+const AppContext = React.createContext();
+
+export const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(true);
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+  const openSubmenu = () => {
+    setIsSubmenuOpen(true);
+  };
+  const closeSubmenu = () => {
+    setIsSubmenuOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        isSidebarOpen,
+        isSubmenuOpen,
+        openSidebar,
+        closeSidebar,
+        openSubmenu,
+        closeSubmenu,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+    return useContext(AppContext)
+}
