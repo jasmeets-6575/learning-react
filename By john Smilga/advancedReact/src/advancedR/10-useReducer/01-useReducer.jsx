bde316b942fa877e606a09e871760cfd44eb52b6
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-03-29 20:21:32 +0530

    useReducer basics

--- By john Smilga/advancedReact/src/advancedR/10-useReducer/01-useReducer.jsx
+++ By john Smilga/advancedReact/src/advancedR/10-useReducer/01-useReducer.jsx
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { useReducer } from "react";
 import { data } from "../../data";
+
+const defaultState = {
+  people:data,
+}
+
+const reducer = ()=> {};
+
+
 const ReducerBasics = () => {
-  const [people, setPeople] = React.useState(data);
+  const [state,dispatch] = useReducer(reducer,defaultState);  
 
   const removeItem = (id) => {
-    let newPeople = people.filter((person) => person.id !== id);
-    setPeople(newPeople);
+    // let newPeople = people.filter((person) => person.id !== id);
+    // setPeople(newPeople);
   };
 
   const resetList = () => {
-    setPeople(data);
+    // setPeople(data);
   };
   return (
     <div>
-      {people.map((person) => {
+      {state.people.map((person) => {
         const { id, name } = person;
         return (
           <div key={id} className="item">
