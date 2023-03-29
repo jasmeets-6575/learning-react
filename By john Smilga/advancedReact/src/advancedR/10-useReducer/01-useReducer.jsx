aaa3cd8360d1346983ef8b7c3a8d8748bd97d2fd
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-03-29 19:34:45 +0530

    useReducer basics

--- By john Smilga/advancedReact/src/advancedR/10-useReducer/01-useReducer.jsx
+++ By john Smilga/advancedReact/src/advancedR/10-useReducer/01-useReducer.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { data } from '../../../data';
+import React from "react";
+import { data } from "../../data";
 const ReducerBasics = () => {
   const [people, setPeople] = React.useState(data);
 
@@ -7,24 +7,35 @@ const ReducerBasics = () => {
     let newPeople = people.filter((person) => person.id !== id);
     setPeople(newPeople);
   };
+
+  const resetList = () => {
+    setPeople(data);
+  };
   return (
     <div>
       {people.map((person) => {
         const { id, name } = person;
         return (
-          <div key={id} className='item'>
+          <div key={id} className="item">
             <h4>{name}</h4>
             <button onClick={() => removeItem(id)}>remove</button>
           </div>
         );
       })}
-      <button
-        className='btn'
-        style={{ marginTop: '2rem' }}
-        onClick={() => setPeople([])}
-      >
-        clear items
-      </button>
+
+      {people.length < 1 ? (
+        <button className="btn" onClick={resetList}>
+          reset
+        </button>
+      ) : (
+        <button
+          className="btn"
+          style={{ marginTop: "2rem" }}
+          onClick={() => setPeople([])}
+        >
+          clear
+        </button>
+      )}
     </div>
   );
 };
