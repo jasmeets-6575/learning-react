ded4d90d7ba0463516956272d0b81ad97650dc64
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-03-30 13:36:05 +0530

    useReducer functionality

--- By john Smilga/advancedReact/src/advancedR/10-useReducer/01-useReducer.jsx
+++ By john Smilga/advancedReact/src/advancedR/10-useReducer/01-useReducer.jsx
@@ -17,6 +17,10 @@ const reducer = (state, action) => {
   if (action.type === RESET_LIST) {
     return { ...state, people: data };
   }
+  if (action.type === REMOVE_ITEM) {
+    let newPeople = state.people.filter((person)=>person.id !== action.payload.id)
+    return {...state, people:newPeople};
+  }
   throw new Error(`No matching "${action.type}" - action type`);
 };
 
@@ -24,7 +28,7 @@ const ReducerBasics = () => {
   const [state, dispatch] = useReducer(reducer, defaultState);
 
   const removeItem = (id) => {
-    dispatch({type:REMOVE_ITEM})
+    dispatch({type:REMOVE_ITEM , payload :{id}});
   };
 
   const clearList = () => {
