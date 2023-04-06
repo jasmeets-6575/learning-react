69335960a5e6f2b637349218cf6da1cdaf4e8534
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-04-06 23:23:12 +0530

    cart project

--- By john Smilga/projects/12-cart/src/context.jsx
+++ By john Smilga/projects/12-cart/src/context.jsx
@@ -1,6 +1,7 @@
-<<<<<<< HEAD
 import { useReducer, createContext, useContext } from "react";
 import reducer from "./reducer";
+import { CLEAR_CART,INCREASE,REMOVE,DECREASE,LOADING,DISPLAY_ITEMS } from "./actions";
+
 const AppContext = createContext();
 
 const initialState = {
@@ -12,23 +13,6 @@ export const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   return <AppContext.Provider value={{...state}}>{children}</AppContext.Provider>;
-=======
-import { createContext , useContext } from "react";
-
-const AppContext = createContext();
-
-const reducer = (state, action) => {
-    return state;
-}
-
-export const AppProvider = ({ children }) => {
-  
-  const [] = useReducer() 
-    
-  return (
-    <AppContext.Provider value={{}}>{children}</AppContext.Provider>
-  );
->>>>>>> 5930027eee88e77409b3df7d0c50c64995afa091
 };
 
 export const useGlobalContext = () => {
--- By john Smilga/projects/12-cart/src/reducer.js
+++ By john Smilga/projects/12-cart/src/reducer.js
@@ -1,3 +1,5 @@
+import { CLEAR_CART,INCREASE,REMOVE,DECREASE,LOADING,DISPLAY_ITEMS } from "./actions";
+
 const reducer = (state, action) => {
     return state;
   };
