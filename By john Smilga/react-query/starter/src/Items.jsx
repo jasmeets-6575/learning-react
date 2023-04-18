ab0b621fc2795df8120081e0c721d1dfcc548509
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-04-18 19:22:45 +0530

    react query

--- By john Smilga/react-query/starter/src/Items.jsx
+++ By john Smilga/react-query/starter/src/Items.jsx
@@ -1,8 +1,20 @@
-import SingleItem from './SingleItem';
-const Items = ({ items }) => {
+import { useQuery } from "@tanstack/react-query";
+import SingleItem from "./SingleItem";
+import customFetch from "./utils";
+
+const Items = () => {
+  const { isLoading, data } = useQuery({
+    queryKey: ["tasks"],
+    queryFn: () => customFetch.get("/"),
+  });
+
+  if (isLoading) {
+    return <p style={{ marginTop: "1rem" }}>Loading...</p>;
+  }
+
   return (
-    <div className='items'>
-      {items.map((item) => {
+    <div className="items">
+      {data.data.taskList.map((item) => {
         return <SingleItem key={item.id} item={item} />;
       })}
     </div>
