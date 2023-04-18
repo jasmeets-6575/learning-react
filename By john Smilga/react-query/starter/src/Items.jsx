c8c5034530f8471f5693ed9e0f8db3369d8b0ab0
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-04-18 20:29:47 +0530

    axios

--- By john Smilga/react-query/starter/src/Items.jsx
+++ By john Smilga/react-query/starter/src/Items.jsx
@@ -3,13 +3,20 @@ import SingleItem from "./SingleItem";
 import customFetch from "./utils";
 
 const Items = () => {
-  const { isLoading, data } = useQuery({
+  const { isLoading, data, error, isError } = useQuery({
     queryKey: ["tasks"],
     queryFn: () => customFetch.get("/"),
   });
 
   if (isLoading) {
     return <p style={{ marginTop: "1rem" }}>Loading...</p>;
+  }
+
+  // if (isError) {
+  //   return <p style={{ marginTop: "1rem" }}>There was an Error...</p>;
+  // }
+  if (error) {
+    return <p style={{ marginTop: "1rem" }}>{error.response.data}</p>;
   }
 
   return (
