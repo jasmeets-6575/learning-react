9b49d130f7f0250c49652b0e1782eba97a139958
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-04-19 15:26:16 +0530

    react query

--- By john Smilga/react-query/starter/src/Form.jsx
+++ By john Smilga/react-query/starter/src/Form.jsx
@@ -1,17 +1,28 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation , useQueryClient} from "@tanstack/react-query";
 import { useState } from "react";
 import customFetch from "./utils";
+import { toast } from "react-toastify";
 
 const Form = () => {
   const [newItemName, setNewItemName] = useState("");
 
+  const queryClient = useQueryClient()
   const { mutate: createTask, isLoading } = useMutation({
     mutationFn: (taskTitle) => customFetch.post("/", { title: taskTitle }),
+    onSuccess:()=>{
+      queryClient.invalidateQueries({queryKey:['tasks']})
+      toast.success('Task Added')
+      setNewItemName('')
+    },
+    onError:(error)=>{
+    toast.error(error.response.data.msg);
+    },
   });
 
   const handleSubmit = (e) => {
     e.preventDefault();
     createTask(newItemName);
+
   };
   return (
     <form onSubmit={handleSubmit}>
