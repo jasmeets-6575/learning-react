a80bfa0ae166ca18e51561ff7f462b29ce691714
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-04-19 22:17:31 +0530

    react query

--- By john Smilga/react-query/starter/src/reactQueryCustomHooks.jsx
+++ By john Smilga/react-query/starter/src/reactQueryCustomHooks.jsx
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import customFetch from "./utils";
 
 export const useFetchTasks = () => {
@@ -11,3 +11,29 @@ export const useFetchTasks = () => {
       });
       return {isLoading,isError,data}
 }
+
+export const useCreateTask = () => {};
+export const useEditTask = () => {
+    const queryClient = useQueryClient()
+    const { mutate: editTask } = useMutation({
+        mutationFn: ({ taskId, isDone }) => {
+          return customFetch.patch(`/${taskId}`, { isDone });
+        },
+        onSuccess: () => {
+          queryClient.invalidateQueries({ queryKey: ["tasks"] });
+        },
+      });
+      return {editTask}
+};
+export const useDeleteTask = () => {
+    const queryClient = useQueryClient()  
+  const { mutate: deleteTask, isLoading } = useMutation({
+    mutationFn: (taskId) => {
+      return customFetch.delete(`/${taskId}`);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["tasks"] });
+    },
+  });
+  return {deleteTask,isLoading}
+};
