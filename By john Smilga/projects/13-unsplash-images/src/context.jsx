defb4e1e450f13916a9b7fbf796ec12cc702c947
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-04-22 13:53:11 +0530

    unsplash project

--- By john Smilga/projects/13-unsplash-images/src/context.jsx
+++ By john Smilga/projects/13-unsplash-images/src/context.jsx
@@ -1,11 +1,21 @@
 import { createContext, useContext, useState, useEffect } from "react";
 
-const AppContext =  createContext()
+const AppContext = createContext();
 
-export const AppProvider = ({children}) => {
-    return <AppContext.Provider value={{}}> 
-    {children}
+export const AppProvider = ({ children }) => {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const toggleDarkTheme = () => {
+    const newDarkTheme = !isDarkTheme;
+    setIsDarkTheme(newDarkTheme);
+    const body = document.querySelector("body");
+    body.classlist.toggle("dark-theme", newDarkTheme);
+    console.log(body);
+  };
+  return (
+    <AppContext.Provider value={{ isDarkTheme, toggleDarkTheme }}>
+      {children}
     </AppContext.Provider>
-}
+  );
+};
 
 export const useGlobalContext = () => useContext(AppContext);
