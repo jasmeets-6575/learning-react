01259b0234b1a2dbe8f980a90b93745390186ef8
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-04-22 13:09:16 +0530

    unsplash project

---
+++ By john Smilga/projects/13-unsplash-images/src/Gallery.jsx
@@ -0,0 +1,6 @@
+const Gallery = () => {
+  return (
+    <h2>Gallery</h2>
+  )
+}
+export default Gallery
---
+++ By john Smilga/projects/13-unsplash-images/src/ThemeToggle.jsx
@@ -0,0 +1,6 @@
+const ThemeToggle = () => {
+  return (
+    <h2>ThemeToggle</h2>
+  )
+}
+export default ThemeToggle
