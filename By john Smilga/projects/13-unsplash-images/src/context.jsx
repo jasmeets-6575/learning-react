b5f09dbfbbdc5546355d48e0d0045abcf0a2c8bc
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-04-23 20:13:03 +0530

    unsplash project

--- By john Smilga/projects/13-unsplash-images/src/context.jsx
+++ By john Smilga/projects/13-unsplash-images/src/context.jsx
@@ -1,17 +1,25 @@
 import { createContext, useContext, useState, useEffect } from "react";
 
 const AppContext = createContext();
+const getInitialDarkMode = () => {
+  const prefersDarkMode = window.matchMedia(
+    "(prefers-color-scheme:dark)"
+  ).matches;
+  return prefersDarkMode;
+};
 
 export const AppProvider = ({ children }) => {
-  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(getInitialDarkMode());
   const [searchTerm, setSearchTerm] = useState("cat");
   const toggleDarkTheme = () => {
     const newDarkTheme = !isDarkTheme;
     setIsDarkTheme(newDarkTheme);
     const body = document.querySelector("body");
     body.classList.toggle("dark-theme", newDarkTheme);
-    console.log(body);
   };
+  useEffect(() => {
+    document.body.classList.toggle("dark-theme", isDarkTheme);
+  }, []);
   return (
     <AppContext.Provider
       value={{ isDarkTheme, toggleDarkTheme, searchTerm, setSearchTerm }}
