80e3657bbfc6e99d131d1e15955ba010bf18adde
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-04-23 16:54:24 +0530

    unsplash project

--- By john Smilga/projects/13-unsplash-images/src/SearchForm.jsx
+++ By john Smilga/projects/13-unsplash-images/src/SearchForm.jsx
@@ -1,6 +1,26 @@
 const SearchForm = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log(e.target.elements);
+    const searchValue = e.target.elements.search.value;
+    if (!searchValue) return;
+    console.log(searchValue);
+  };
   return (
-    <h2>SearchForm</h2>
-  )
-}
-export default SearchForm
+    <section>
+      <h1 className="title">unsplash images</h1>
+      <form className="search-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="form-input search-input"
+          name="search"
+          placeholder="cat"
+        />
+        <button type="submit" className="btn">
+          search
+        </button>
+      </form>
+    </section>
+  );
+};
+export default SearchForm;
