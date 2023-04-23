bec3e5247096f3a901a386f5ddd4d5ec70696112
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-04-23 18:42:53 +0530

    unsplash project

--- By john Smilga/projects/13-unsplash-images/src/Gallery.jsx
+++ By john Smilga/projects/13-unsplash-images/src/Gallery.jsx
@@ -1,7 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 const url =
-  "https://api.unsplash.com/search/photos?client_id=v-iPJA8sHq8yjEUd7pXPkDZ-PKHvl4RnfxfrJ_M8RMM&query=office";
+  "https://api.unsplash.com/search/photos?client_id=v-iPJA8sHq8yjEUd7pXPkDZ-PKHvl4RnfxfrJ_M8RMM&query=cat";
 
 const Gallery = () => {
   const response = useQuery({
@@ -12,6 +13,44 @@ const Gallery = () => {
       return result.data;
     },
   });
-  return <h2>Gallery</h2>;
+  if (response.isLoading) {
+    return (
+      <section className="image-container">
+        <h3>Loading...</h3>
+      </section>
+    );
+  }
+  if (response.isError) {
+    return (
+      <section className="image-container">
+        <h3>There was an Error...</h3>
+      </section>
+    );
+  }
+
+  const results = response.data.results;
+  if (results.length < 1) {
+    return (
+      <section className="image-container">
+        <h3>No results found... </h3>
+      </section>
+    );
+  }
+
+  return (
+    <section className="image-container">
+      {results.map((item) => {
+        const url = item?.urls?.regular;
+        return (
+          <img
+            src={url}
+            key={item.id}
+            alt={item.alt_description}
+            className="img"
+          ></img>
+        );
+      })}
+    </section>
+  );
 };
 export default Gallery;
