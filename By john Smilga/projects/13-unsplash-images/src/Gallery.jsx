277633465e472187e87e349c82ccfb60194ea64f
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-04-23 18:20:23 +0530

    unsplash project

--- By john Smilga/projects/13-unsplash-images/src/Gallery.jsx
+++ By john Smilga/projects/13-unsplash-images/src/Gallery.jsx
@@ -1,6 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
+
+const url =
+  "https://api.unsplash.com/search/photos?client_id=v-iPJA8sHq8yjEUd7pXPkDZ-PKHvl4RnfxfrJ_M8RMM&query=office";
+
 const Gallery = () => {
-  return (
-    <h2>Gallery</h2>
-  )
-}
-export default Gallery
+  const response = useQuery({
+    queryKey: ["images"],
+    queryFn: async () => {
+      const result = await axios.get(url);
+
+      return result.data;
+    },
+  });
+  return <h2>Gallery</h2>;
+};
+export default Gallery;
