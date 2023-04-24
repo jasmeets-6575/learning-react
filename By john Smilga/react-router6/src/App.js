aa8ef8da535100e519869c996e2e1e9c15f19723
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-04-24 11:16:38 +0530

    React Router 6

--- By john Smilga/react-router6/src/App.js
+++ By john Smilga/react-router6/src/App.js
@@ -1,10 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Products from "./pages/Products"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div>home page</div>} />
+        <Route path="/" element={<Home/>} />
+        <Route path="about" element={<About/>} />
+        <Route path="products" element={<Products/>} />
       </Routes>
     </BrowserRouter>
   );
