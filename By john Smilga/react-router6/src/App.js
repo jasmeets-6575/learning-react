efd90b354707a02ddc3e6c9e340de106aaadd430
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-04-24 12:23:33 +0530

    React Router 6

--- By john Smilga/react-router6/src/App.js
+++ By john Smilga/react-router6/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SharedLayout from "./pages/SharedLayout"
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Error from "./pages/Error";
@@ -8,7 +9,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Home/>}/>
           <Route path="about" element={<About />} />
           <Route path="products" element={<Products />} />
           <Route path="*" element={<Error />} />
---
+++ By john Smilga/react-router6/src/pages/SharedLayout.js
@@ -0,0 +1,12 @@
+import { Link, Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
+
+const SharedLayout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
+};
+export default SharedLayout;
