a3979b7213aa8d728921fe9f082c8382f6375023
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-04-24 22:29:43 +0530

    React Router 6

--- By john Smilga/react-router6/src/pages/Products.js
+++ By john Smilga/react-router6/src/pages/Products.js
@@ -1,10 +1,21 @@
+import { Link } from "react-router-dom";
+import products from "../data";
+
 const Products = () => {
   return (
-    <>
-      <section className='section'>
-        <h2>products</h2>
-      </section>
-    </>
+    <section className="section">
+      <h2>products</h2>
+      <div className="products">
+        {products.map((product) => {
+          return (
+            <article key={product.id}>
+              <h4>{product.name}</h4>
+              <Link to={`/products/${product.id}`}>more info</Link>
+            </article>
+          );
+        })}
+      </div>
+    </section>
   );
 };
 
--- By john Smilga/react-router6/src/pages/SingleProduct.js
+++ By john Smilga/react-router6/src/pages/SingleProduct.js
@@ -1,10 +1,14 @@
-import { Link,useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import products from "../data";
 
 const SingleProduct = () => {
-  const {productId} = useParams()
+  const { productId } = useParams();
+  const product = products.find((product) => product.id === productId);
+  const { image, name } = product;
   return (
-    <section className='section product'>
-      <h4>{productId}</h4>
+    <section className="section product">
+      <img src={image} alt={name} />
+      <h4>{name}</h4>
       <Link to="/products">back to Products</Link>
     </section>
   );
