09f28c303cdae1fb73fb3e2ae60f4a408bf53654
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-04-24 23:05:40 +0530

    React Router 6

--- By john Smilga/react-router6/src/App.js
+++ By john Smilga/react-router6/src/App.js
@@ -1,12 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 import SharedLayout from "./pages/SharedLayout";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Error from "./pages/Error";
 import Products from "./pages/Products";
 import SingleProduct from "./pages/SingleProduct";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
+  const [user, setUser] = useState(null);
   return (
     <BrowserRouter>
       <Routes>
@@ -15,6 +19,8 @@ function App() {
           <Route path="about" element={<About />} />
           <Route path="products" element={<Products />} />
           <Route path="products/:productId" element={<SingleProduct />} />
+          <Route path="login" element={<Login setUser={setUser} />} />
+          <Route path="dashboard" element={<Dashboard user={user} />} />
           <Route path="*" element={<Error />} />
         </Route>
       </Routes>
--- By john Smilga/react-router6/src/components/StyledNavbar.js
+++ By john Smilga/react-router6/src/components/StyledNavbar.js
@@ -21,6 +21,12 @@ const StyledNavbar = () => {
       >
         Products
       </NavLink>
+      <NavLink
+        to="/login"
+        className={({ isActive }) => (isActive ? "link active" : "link")}
+      >
+        Login
+      </NavLink>
     </nav>
   );
 };
