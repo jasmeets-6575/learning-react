b0e78787ac8210e62eb5efc2cec78fdfa9e762dd
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-04-27 20:32:17 +0530

    Comfy project adding format price

--- By john Smilga/projects/15-Comfy-sloth/src/utils/helpers.js
+++ By john Smilga/projects/15-Comfy-sloth/src/utils/helpers.js
@@ -1,3 +1,8 @@
-export const formatPrice = () => {}
+export const formatPrice = (number) => {
+  return new Intl.NumberFormat("en-Us", {
+    style: "currency",
+    currency: "USD",
+  }).format(number / 100);
+};
 
-export const getUniqueValues = () => {}
+export const getUniqueValues = () => {};
