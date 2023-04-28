ca90159a12424c55ac2b8350983120c7a1e2935f
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-04-28 20:03:01 +0530

    Comfy project adding product page filters

--- By john Smilga/projects/15-Comfy-sloth/src/context/filter_context.js
+++ By john Smilga/projects/15-Comfy-sloth/src/context/filter_context.js
@@ -17,6 +17,15 @@ const initialState = {
   all_products: [],
   grid_view: true,
   sort: "price-lowest",
+  filters:{
+    text:'',
+    company:'all',
+    category:'all',
+    color:'all',
+    min_price:0,
+    max_price:0,
+    price:0,
+  }
 };
 
 const FilterContext = React.createContext();
