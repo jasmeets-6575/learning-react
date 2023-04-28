3ad909b0fd1878ecd29dd97d76b3900f0663f4bd
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-04-28 22:17:39 +0530

    Comfy project adding product page filter companies

--- By john Smilga/projects/15-Comfy-sloth/src/components/Filters.js
+++ By john Smilga/projects/15-Comfy-sloth/src/components/Filters.js
@@ -59,6 +59,26 @@ const Filters = () => {
             </div>
           </div>
           {/* end ofcategories */}
+
+          {/* Companies */}
+          <div className="form-control">
+            <h5>company</h5>
+            <select
+              name="company"
+              value={company}
+              onChange={updateFilters}
+              className="company"
+            >
+              {companies.map((c, index) => {
+                return (
+                  <option key={index} value={c}>
+                    {c}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          {/* end of Companies */}
         </form>
       </div>
     </Wrapper>
