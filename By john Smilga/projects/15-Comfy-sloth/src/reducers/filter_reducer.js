19385cc33518a1d8e3af745edc51428d6636a512
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-04-29 00:17:03 +0530

    Comfy project adding product page filter functionality

--- By john Smilga/projects/15-Comfy-sloth/src/reducers/filter_reducer.js
+++ By john Smilga/projects/15-Comfy-sloth/src/reducers/filter_reducer.js
@@ -60,11 +60,39 @@ const filter_reducer = (state, action) => {
 
     let tempProducts = [...all_products];
     //filtering
-    if(text){
-      tempProducts = tempProducts.filter((product)=>{
-        return product.name.toLowerCase().startsWith(text)
-      })
+    //text
+    if (text) {
+      tempProducts = tempProducts.filter((product) => {
+        return product.name.toLowerCase().startsWith(text);
+      });
     }
+    //category
+    if (category !== "all") {
+      tempProducts = tempProducts.filter(
+        (product) => product.category === category
+      );
+    }
+    //company
+    if (company !== "all") {
+      tempProducts = tempProducts.filter(
+        (product) => product.company === company
+      );
+    }
+    //color
+    if (color !== "all") {
+      tempProducts = tempProducts.filter((product) => {
+        return product.colors.find((c) => c === color);
+      });
+    }
+    //price
+    tempProducts = tempProducts.filter((product) => product.price <= price);
+    //shipping
+    if (shipping) {
+      tempProducts = tempProducts.filter(
+        (product) => product.shipping === true
+      );
+    }
+
     return { ...state, filtered_products: tempProducts };
   }
   if (action.type === CLEAR_FILTERS) {
