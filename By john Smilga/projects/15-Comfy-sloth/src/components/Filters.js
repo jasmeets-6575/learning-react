d4389501601a0f6b86bbcd259e478e11219a8736
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-04-28 22:45:59 +0530

    Comfy project adding product page filter colors

--- By john Smilga/projects/15-Comfy-sloth/src/components/Filters.js
+++ By john Smilga/projects/15-Comfy-sloth/src/components/Filters.js
@@ -79,6 +79,45 @@ const Filters = () => {
             </select>
           </div>
           {/* end of Companies */}
+
+          {/* colors */}
+          <div className="form-control">
+            <h5>colors</h5>
+            <div className="colors">
+              {colors.map((c, index) => {
+                if (c === "all") {
+                  return (
+                    <button
+                      key={index}
+                      name="color"
+                      onClick={updateFilters}
+                      data-color="all"
+                      className={`${
+                        color === c ? "all-btn active" : "all-btn"
+                      }`}
+                    >
+                      all
+                    </button>
+                  );
+                }
+                return (
+                  <button
+                    key={index}
+                    name="color"
+                    style={{ background: c }}
+                    className={`${
+                      color === c ? "color-btn active" : "color-btn"
+                    }`}
+                    data-color={c}
+                    onClick={updateFilters}
+                  >
+                    {color === c ? <FaCheck /> : null}
+                  </button>
+                );
+              })}
+            </div>
+          </div>
+          {/* end of colors */}
         </form>
       </div>
     </Wrapper>
