011023eba547393e1d29d836d1c3a2e8bd7a3668
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-04-29 04:07:38 +0530

    Comfy project adding auth0

--- By john Smilga/projects/15-Comfy-sloth/src/components/CartButtons.js
+++ By john Smilga/projects/15-Comfy-sloth/src/components/CartButtons.js
@@ -9,17 +9,21 @@ import { useUserContext } from "../context/user_context";
 const CartButtons = () => {
   const { closeSidebar } = useProductsContext();
   const {total_items} = useCartContext()
+  const {loginWithRedirect, logout, myUser} = useUserContext()
   return (
     <Wrapper className="cart-btn-wrapper">
       <Link to="/cart" className="cart-btn" onClick={closeSidebar}>
         Cart
         <span className="cart-container">
           <FaShoppingCart />
-          <span className="cart-value">total_items</span>
+          <span className="cart-value">{total_items}</span>
         </span>
       </Link>
-      <button type="button" className="auth-btn">
+      <button type="button" className="auth-btn" onClick={loginWithRedirect}>
         Login <FaUserPlus />
+      </button>
+      <button type="button" className="auth-btn" onClick={()=>logout({returnTo:window.location.origin})}>
+        Logout <FaUserMinus />
       </button>
     </Wrapper>
   );
--- By john Smilga/projects/15-Comfy-sloth/src/index.js
+++ By john Smilga/projects/15-Comfy-sloth/src/index.js
@@ -8,14 +8,27 @@ import { CartProvider } from "./context/cart_context";
 import { UserProvider } from "./context/user_context";
 import { Auth0Provider } from "@auth0/auth0-react";
 
+//jasmeets-6575.us.auth0.com
+//XjQtWyrtRYsX76AfNqw4YP6yRLAYcSoB
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>
+  <Auth0Provider
+    domain="jasmeets-6575.us.auth0.com"
+    clientId="XjQtWyrtRYsX76AfNqw4YP6yRLAYcSoB"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>
 );
