ace84ba0436dfa66fbfe2e3feda1b31c5c53cb55
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-04-28 14:04:15 +0530

    Comfy project adding product page

--- By john Smilga/projects/15-Comfy-sloth/src/index.js
+++ By john Smilga/projects/15-Comfy-sloth/src/index.js
@@ -12,6 +12,8 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <ProductsProvider>
-    <App />
+    <FilterProvider>
+      <App />
+    </FilterProvider>
   </ProductsProvider>
 );
