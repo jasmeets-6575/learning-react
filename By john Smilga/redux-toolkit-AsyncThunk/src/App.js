fd34e191d990daf444875d2672488b0bc678d73e
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-04-30 22:55:11 +0530

    redux toolkit using AsyncThunk

---
+++ By john Smilga/redux-toolkit-AsyncThunk/src/App.js
@@ -0,0 +1,36 @@
+import { useDispatch, useSelector } from "react-redux";
+import { calculateTotals, getCartitems } from "./features/cart/cartSlice";
+import { useEffect } from "react";
+import Modal from "./components/Modal";
+import Navbar from "./components/Navbar";
+import CartContainer from "./components/CartContainer";
+
+function App() {
+  const { cartItems, isLoading } = useSelector((store) => store.cart);
+  const { isOpen } = useSelector((store) => store.modal);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
+  useEffect(() => {
+    dispatch(getCartitems());
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
+  return (
+    <main>
+      {isOpen && <Modal />}
+      <Navbar />
+      <CartContainer />
+    </main>
+  );
+}
+export default App;
