4c442865f906093f9e3be8573b6c78af35149c6a
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-04-30 16:29:46 +0530

    redux toolkit adding cart item

--- By john Smilga/redux-toolkit/src/components/CartContainer.js
+++ By john Smilga/redux-toolkit/src/components/CartContainer.js
@@ -1,7 +1,8 @@
 import CartItem from "./CartItem";
-import { useSelector } from "react-redux";
-
+import { useDispatch, useSelector } from "react-redux";
+import { clearCart } from "../features/cart/cartSlice";
 const CartContainer = () => {
+  const dispatch = useDispatch();
   const { cartItems, total, amount } = useSelector((store) => store.cart);
 
   if (amount < 1) {
@@ -31,7 +32,11 @@ const CartContainer = () => {
             total <span>${total}</span>
           </h4>
         </div>
-        <button type="button" className="btn clear">
+        <button
+          type="button"
+          className="btn clear"
+          onClick={() => dispatch(clearCart())}
+        >
           clear cart
         </button>
       </footer>
--- By john Smilga/redux-toolkit/src/features/cart/cartSlice.js
+++ By john Smilga/redux-toolkit/src/features/cart/cartSlice.js
@@ -1,15 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
-import cartItems from "../../cartItems"
+import cartItems from "../../cartItems";
 const initialState = {
-    cartItems : cartItems,
-    amount : cartItems.length,
-    total : 0,
-    isLoading : true,
+  cartItems: cartItems,
+  amount: cartItems.length,
+  total: 0,
+  isLoading: true,
 };
 console.log(cartItems);
 const cartSlice = createSlice({
-    name:'cart',
-    initialState,
-})
-
-export default cartSlice.reducer
+  name: "cart",
+  initialState,
+  reducers: {
+    clearCart: (state) => {
+      state.cartItems = [];
+    },
+  },
+});
+export const { clearCart } = cartSlice.actions;
+export default cartSlice.reducer;
