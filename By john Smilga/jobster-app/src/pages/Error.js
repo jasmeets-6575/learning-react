49bfbaa08cf996bfd75cf9eba8e56c8ddf6a358e
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-05-02 20:19:19 +0530

    jobster basic register page

--- By john Smilga/jobster-app/src/pages/Error.js
+++ By john Smilga/jobster-app/src/pages/Error.js
@@ -1,7 +1,6 @@
-import { Link } from "react-router-dom"
-import img from "../assets/images/not-found.svg"
-import Wrapper from "../assets/wrappers/ErrorPage"
-
+import { Link } from "react-router-dom";
+import img from "../assets/images/not-found.svg";
+import Wrapper from "../assets/wrappers/ErrorPage";
 
 const Error = () => {
   return (
@@ -10,9 +9,9 @@ const Error = () => {
         <img src={img} alt="not found" />
         <h3>Ohh! Page Not Found</h3>
         <p>We can't seem to find the page you're looking for</p>
-        <Link to="/" >back home</Link>
+        <Link to="/">back home</Link>
       </div>
     </Wrapper>
-  )
-}
-export default Error
+  );
+};
+export default Error;
--- By john Smilga/jobster-app/src/pages/Register.js
+++ By john Smilga/jobster-app/src/pages/Register.js
@@ -1,6 +1,53 @@
+import { useState, useEffect } from "react";
+import { FormRow, Logo } from "../components";
+import Wrapper from "../assets/wrappers/RegisterPage";
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+};
+
 const Register = () => {
+  const [values, setValues] = useState(initialState);
+
+  const handleChange = (e) => {
+    console.log(e.target);
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+
   return (
-    <div>Register</div>
-  )
-}
-export default Register
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={onSubmit}>
+        <Logo />
+        <h3>Login</h3>
+        <FormRow
+          type="text"
+          name="name"
+          value={values.name}
+          handleChange={handleChange}
+        />
+        <FormRow
+          type="email"
+          name="email"
+          value={values.email}
+          handleChange={handleChange}
+        />
+        <FormRow
+          type="password"
+          name="password"
+          value={values.password}
+          handleChange={handleChange}
+        />
+        <button type="submit" className="btn btn-block">
+          Submit
+        </button>
+      </form>
+    </Wrapper>
+  );
+};
+export default Register;
