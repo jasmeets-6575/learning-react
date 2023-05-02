7991833b5a56a4e7f212a06e7564b978b81ead01
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-05-02 21:45:56 +0530

    jobster basic register page

--- By john Smilga/jobster-app/src/pages/Register.js
+++ By john Smilga/jobster-app/src/pages/Register.js
@@ -19,24 +19,31 @@ const Register = () => {
     e.preventDefault();
     console.log(e.target);
   };
-
+  const toggleMember = () => {
+    setValues({ ...values, isMember: !values.isMember });
+  };
   return (
     <Wrapper className="full-page">
       <form className="form" onSubmit={onSubmit}>
         <Logo />
-        <h3>Login</h3>
-        <FormRow
-          type="text"
-          name="name"
-          value={values.name}
-          handleChange={handleChange}
-        />
+        <h3>{values.isMember ? "Login" : "Register"}</h3>
+
+        {!values.isMember && (
+          <FormRow
+            type="text"
+            name="name"
+            value={values.name}
+            handleChange={handleChange}
+          />
+        )}
+
         <FormRow
           type="email"
           name="email"
           value={values.email}
           handleChange={handleChange}
         />
+
         <FormRow
           type="password"
           name="password"
@@ -46,6 +53,12 @@ const Register = () => {
         <button type="submit" className="btn btn-block">
           Submit
         </button>
+        <p>
+          {values.isMember ? "Not a member yet?" : "Already a member?"}
+          <button className="member-btn" onClick={toggleMember}>
+          {!values.isMember ? "Login" :"Register" }
+          </button>
+        </p>
       </form>
     </Wrapper>
   );
