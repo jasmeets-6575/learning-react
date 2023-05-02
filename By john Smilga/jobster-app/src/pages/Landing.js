2e000ffe7daf41a13373c3ecddf1b56f08c91cc5
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-05-02 15:56:00 +0530

    jobster basic

--- By john Smilga/jobster-app/src/pages/Landing.js
+++ By john Smilga/jobster-app/src/pages/Landing.js
@@ -1,12 +1,11 @@
-import logo from "../assets/images/logo.svg";
 import main from "../assets/images/main.svg";
-import styled from "styled-components";
-
+import Wrapper from "../assets/wrappers/LandingPage";
+import {Logo} from "../components";
 const Landing = () => {
   return (
     <Wrapper>
       <nav>
-        <img src={logo} alt="jobster logo" className="logo" />
+        <Logo/>
       </nav>
       <div className="container page">
         <div className="info">
@@ -25,43 +24,5 @@ const Landing = () => {
     </Wrapper>
   );
 };
-
-const Wrapper = styled.main`
-  nav {
-    width: var(--fluid-width);
-    max-width: var(--max-width);
-    margin: 0 auto;
-    height: var(--nav-height);
-    display: flex;
-    align-items: center;
-  }
-  .page {
-    min-height: calc(100vh - var(--nav-height));
-    display: grid;
-    align-items: center;
-    margin-top: -1.5rem;
-  }
-  h1 {
-    font-weight: 700;
-    span {
-      color: var(--primary-500);
-    }
-  }
-  p {
-    color: var(--grey-600);
-  }
-  .main-img {
-    display: none;
-  }
-  @media (min-width: 992px) {
-    .page {
-      grid-template-columns: 1fr 1fr;
-      column-gap: 3rem;
-    }
-    .main-img {
-      display: block;
-    }
-  }
-`;
 
 export default Landing;
