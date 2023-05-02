0f2f1f6ab71c7ec655b090a3c6534ad0b58d18fa
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-05-02 18:37:39 +0530

    jobster basic Error page

--- By john Smilga/jobster-app/src/App.js
+++ By john Smilga/jobster-app/src/App.js
@@ -1,9 +1,16 @@
-import Landing from "./pages/Landing";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Landing, Error, Dashboard, Register } from "./pages";
+
 function App() {
   return (
-    <main>
-      <Landing />
-    </main>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/landing" element={<Landing />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- By john Smilga/jobster-app/src/pages/Landing.js
+++ By john Smilga/jobster-app/src/pages/Landing.js
@@ -1,6 +1,7 @@
 import main from "../assets/images/main.svg";
 import Wrapper from "../assets/wrappers/LandingPage";
 import {Logo} from "../components";
+import { Link } from "react-router-dom";
 const Landing = () => {
   return (
     <Wrapper>
@@ -17,7 +18,7 @@ const Landing = () => {
             voluptatem neque. Perferendis distinctio reiciendis iure corporis
             laboriosam eligendi doloribus cum officiis natus optio, iusto saepe.
           </p>
-          <button className="btn btn-hero">Login / Register</button>
+          <Link to="/register" className="btn btn-hero">Login / Register</Link>
         </div>
         <img src={main} alt="job hunt" className="img main-img" />
       </div>
