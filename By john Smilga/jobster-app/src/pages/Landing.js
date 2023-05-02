0ec3f55d9514b5eb87b8dcb9f5d5d72ca977ff39
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-05-02 15:22:44 +0530

    jobster setup

--- By john Smilga/jobster-app/src/pages/Landing.js
+++ By john Smilga/jobster-app/src/pages/Landing.js
@@ -1,9 +1,67 @@
-import logo from "../assets/images/logo.svg"
-import main from "../assets/images/main.svg"
+import logo from "../assets/images/logo.svg";
+import main from "../assets/images/main.svg";
+import styled from "styled-components";
 
 const Landing = () => {
   return (
-    <h1>Landing page</h1>
-  )
-} 
-export default Landing
+    <Wrapper>
+      <nav>
+        <img src={logo} alt="jobster logo" className="logo" />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app{" "}
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni,
+            voluptatem neque. Perferendis distinctio reiciendis iure corporis
+            laboriosam eligendi doloribus cum officiis natus optio, iusto saepe.
+          </p>
+          <button className="btn btn-hero">Login / Register</button>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.main`
+  nav {
+    width: var(--fluid-width);
+    max-width: var(--max-width);
+    margin: 0 auto;
+    height: var(--nav-height);
+    display: flex;
+    align-items: center;
+  }
+  .page {
+    min-height: calc(100vh - var(--nav-height));
+    display: grid;
+    align-items: center;
+    margin-top: -1.5rem;
+  }
+  h1 {
+    font-weight: 700;
+    span {
+      color: var(--primary-500);
+    }
+  }
+  p {
+    color: var(--grey-600);
+  }
+  .main-img {
+    display: none;
+  }
+  @media (min-width: 992px) {
+    .page {
+      grid-template-columns: 1fr 1fr;
+      column-gap: 3rem;
+    }
+    .main-img {
+      display: block;
+    }
+  }
+`;
+
+export default Landing;
