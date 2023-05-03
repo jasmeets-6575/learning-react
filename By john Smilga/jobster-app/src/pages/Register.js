82e2fc013b2d0a87bc84d809b7cdbae16041cb6d
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-05-04 00:08:40 +0530

    jobster http request

--- By john Smilga/jobster-app/src/pages/Register.js
+++ By john Smilga/jobster-app/src/pages/Register.js
@@ -2,6 +2,9 @@ import { useState, useEffect } from "react";
 import { FormRow, Logo } from "../components";
 import Wrapper from "../assets/wrappers/RegisterPage";
 import { toast } from "react-toastify";
+import { useDispatch, useSelector } from "react-redux";
+import { loginUser, registerUser } from "../features/user/userSlice";
+
 const initialState = {
   name: "",
   email: "",
@@ -11,22 +14,33 @@ const initialState = {
 
 const Register = () => {
   const [values, setValues] = useState(initialState);
+  const { user, isLoading } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
 
   const handleChange = (e) => {
     const name = e.target.name;
     const value = e.target.value;
-    setValues({...values,[name]:value})
+    setValues({ ...values, [name]: value });
   };
+
   const onSubmit = (e) => {
     e.preventDefault();
-    const {name,email,password,isMember} = values;
-    if(!email || !password || (!isMember && !name)){
+    const { name, email, password, isMember } = values;
+    if (!email || !password || (!isMember && !name)) {
       toast.error("please fill out all fields");
+      return;
     }
+    if (isMember) {
+      dispatch(loginUser({ email: email, password: password }));
+      return;
+    }
+    dispatch(registerUser({ name, email, password }));
   };
+
   const toggleMember = () => {
     setValues({ ...values, isMember: !values.isMember });
   };
+
   return (
     <Wrapper className="full-page">
       <form className="form" onSubmit={onSubmit}>
@@ -55,13 +69,13 @@ const Register = () => {
           value={values.password}
           handleChange={handleChange}
         />
-        <button type="submit" className="btn btn-block">
+        <button type="submit" className="btn btn-block" >
           Submit
         </button>
         <p>
-          {values.isMember ? "Not a member yet?" : "Already a member?"}
-          <button className="member-btn" onClick={toggleMember}>
-          {!values.isMember ? "Login" :"Register" }
+          {values.isMember ? 'Not a member yet?' : 'Already a member?'}
+          <button type='button' onClick={toggleMember} className='member-btn'>
+            {values.isMember ? 'Register' : 'Login'}
           </button>
         </p>
       </form>
