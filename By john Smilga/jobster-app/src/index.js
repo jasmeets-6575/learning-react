e17daea9e190d59abb4a244deb1fe57db6ac23b4
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-05-04 01:47:05 +0530

    jobster http request

--- By john Smilga/jobster-app/src/index.js
+++ By john Smilga/jobster-app/src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import 'normalize.css'
-import App from './App';
-import "./index.css"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "normalize.css";
+import App from "./App";
+import "./index.css";
 import { store } from "./Store";
 import { Provider } from "react-redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
   </Provider>
 );
