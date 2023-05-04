81a0eacb2a9db30f4b6573edaf6e5d1401764d0d
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-05-04 15:08:50 +0530

    jobster adding Navbar

--- By john Smilga/jobster-app/src/components/Navbar.js
+++ By john Smilga/jobster-app/src/components/Navbar.js
@@ -1,9 +1,45 @@
 import Wrapper from "../assets/wrappers/Navbar";
 import { FaAlignLeft, FaUserCircle, FaCaretDown } from "react-icons/fa";
+import Logo from "./Logo";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
 const Navbar = () => {
+  const { user } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
   return (
     <Wrapper>
-      <h2>Navbar</h2>
+      <div className="nav-center">
+        <button
+          type="button"
+          className="toggle-btn"
+          onClick={() => console.log("toggle sidebar")}
+        >
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h3 className="logo-text">Dashboard</h3>
+        </div>
+        <div className="btn-container">
+          <button
+            type="button"
+            className="btn"
+            onClick={() => console.log("toggle logout dropdown")}
+          >
+            <FaUserCircle />
+            {user?.name}
+            <FaCaretDown />
+          </button>
+          <div className="dropdown show-dropdown">
+            <button
+              type="button"
+              className="dropdown-btn"
+              onClick={() => console.log("logout")}
+            >Logout</button>
+          </div>
+        </div>
+      </div>
     </Wrapper>
   );
 };
