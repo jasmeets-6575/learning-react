84a049b097ec4c725d24d742d2a8b1023c8fa842
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-05-04 19:13:17 +0530

    jobster adding small sidebar

--- By john Smilga/jobster-app/src/components/SmallSidebar.js
+++ By john Smilga/jobster-app/src/components/SmallSidebar.js
@@ -1,12 +1,24 @@
-import Wrapper from '../assets/wrappers/SmallSidebar';
-import { FaTimes } from 'react-icons/fa';
-import { NavLink } from 'react-router-dom';
-import Logo from './Logo';
-import { useSelector, useDispatch } from 'react-redux';
+import Wrapper from "../assets/wrappers/SmallSidebar";
+import { FaTimes } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
+import Logo from "./Logo";
+import { useSelector, useDispatch } from "react-redux";
 
 const SmallSidebar = () => {
   return (
-    <Wrapper>SmallSidebar</Wrapper>
-  )
-}
-export default SmallSidebar
+    <Wrapper>
+      <div className="sidebar-container show-sidebar">
+        <div className="content">
+          <button className="close-btn" onClick={() => console.log("toggle")}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className="nav-links">nav links</div>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
+export default SmallSidebar;
