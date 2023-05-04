d628a685ffbfa1022c49453ad685cd930fd0c2b9
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-05-04 19:47:40 +0530

    jobster adding small sidebar

--- By john Smilga/jobster-app/src/components/SmallSidebar.js
+++ By john Smilga/jobster-app/src/components/SmallSidebar.js
@@ -3,19 +3,49 @@ import { FaTimes } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
 import Logo from "./Logo";
 import { useSelector, useDispatch } from "react-redux";
+import { toggleSidebar } from "../features/user/userSlice";
+import links from "../utils/links"
+
 
 const SmallSidebar = () => {
+  const { isSidebarOpen } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+
+  const toggle = () => {
+    dispatch(toggleSidebar());
+  };
   return (
     <Wrapper>
-      <div className="sidebar-container show-sidebar">
+      <div
+        className={
+          isSidebarOpen ? "sidebar-container show-sidebar" : "sidebar-container"
+        }
+      >
         <div className="content">
-          <button className="close-btn" onClick={() => console.log("toggle")}>
+          <button className="close-btn" onClick={toggle}>
             <FaTimes />
           </button>
           <header>
             <Logo />
           </header>
-          <div className="nav-links">nav links</div>
+          <div className="nav-links">
+            {links.map((link) => {
+              const { text, path, id, icon } = link;
+              return (
+                <NavLink
+                  key={id}
+                  to={path}
+                  className={({ isActive }) => {
+                    return isActive ? "nav-link active" : "nav-link";
+                  }}
+                  onClick={toggle}
+                >
+                  <span className="icon">{icon}</span>
+                  {text}
+                </NavLink>
+              );
+            })}
+          </div>
         </div>
       </div>
     </Wrapper>
