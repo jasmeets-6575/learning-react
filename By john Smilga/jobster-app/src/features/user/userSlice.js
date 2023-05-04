c6a5ca29f49266094567827fca73e71828a475d5
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-05-04 22:41:49 +0530

    jobster adding profile page

--- By john Smilga/jobster-app/src/features/user/userSlice.js
+++ By john Smilga/jobster-app/src/features/user/userSlice.js
@@ -37,13 +37,30 @@ export const loginUser = createAsyncThunk(
   }
 );
 
+export const updateUser = createAsyncThunk(
+  "user/updateUser",
+  async (user, thunkAPI) => {
+    try {
+      const resp = await customFetch.patch("/auth/updateUser", user, {
+        headers: {
+          authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
+        },
+      });
+      return resp.data;
+    } catch (error) {
+      console.log(error.response);
+      return thunkAPI.rejectWithValue(error.response.data.msg);
+    }
+  }
+);
+
 const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
-    logoutUser:(state) => {
-      state.user=null;
-      state.isSidebarOpen=false;
+    logoutUser: (state) => {
+      state.user = null;
+      state.isSidebarOpen = false;
       removeUserFromLocalStorage();
     },
     toggleSidebar: (state) => {
@@ -79,10 +96,24 @@ const userSlice = createSlice({
       .addCase(loginUser.rejected, (state, { payload }) => {
         state.isLoading = false;
         toast.error(payload);
+      })
+      .addCase(updateUser.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(updateUser.fulfilled, (state, { payload }) => {
+        const { user } = payload;
+        state.isLoading = false;
+        state.user = user;
+        addUserToLocalStorage(user);
+        toast.success("User updated");
+      })
+      .addCase(updateUser.rejected, (state, { payload }) => {
+        state.isLoading = false;
+        toast.error(payload);
       });
   },
 });
 
-export const {toggleSidebar,logoutUser} = userSlice.actions;
+export const { toggleSidebar, logoutUser } = userSlice.actions;
 
 export default userSlice.reducer;
--- By john Smilga/jobster-app/src/pages/dashboard/Profile.js
+++ By john Smilga/jobster-app/src/pages/dashboard/Profile.js
@@ -1,6 +1,71 @@
+import { useState } from "react";
+import { FormRow } from "../../components";
+import Wrapper from "../../assets/wrappers/DashboardFormPage";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import { updateUser } from "../../features/user/userSlice";
 const Profile = () => {
+  const { isLoading, user } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+  const [userData, setUserData] = useState({
+    name: user?.name || "",
+    email: user?.email || "",
+    lastName: user?.lastName || "",
+    location: user?.location || "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { name, email, lastName, location } = userData;
+    if (!name || !email || !lastName || !location) {
+      toast.error("Please fill out all fields");
+      return;
+    }
+    dispatch(updateUser({ name, email, lastName, location }));
+  };
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setUserData({ ...userData, [name]: value });
+  };
+
   return (
-    <div>Profile</div>
-  )
-}
-export default Profile
+    <Wrapper>
+      <form className="form" onSubmit={handleSubmit}>
+        <h3>profile</h3>
+        <div className="form-center">
+          <FormRow
+            type="text"
+            name="name"
+            value={userData.name}
+            handleChange={handleChange}
+          />
+          <FormRow
+            type="text"
+            labelText="last name"
+            name="lastName"
+            value={userData.lastName}
+            handleChange={handleChange}
+          />
+          <FormRow
+            type="email"
+            name="email"
+            value={userData.email}
+            handleChange={handleChange}
+          />
+          <FormRow
+            type="text"
+            name="location"
+            value={userData.location}
+            handleChange={handleChange}
+          />
+          <button className="btn btn-block" type="submit" disabled={isLoading}>
+            {isLoading ? "Please Wait..." : "save changes"}
+          </button>
+        </div>
+      </form>
+    </Wrapper>
+  );
+};
+export default Profile;
