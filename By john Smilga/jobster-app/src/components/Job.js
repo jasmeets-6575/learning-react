cb2bd5d23225994c6979ae7268e3de81a26618eb
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-05-05 22:06:19 +0530

    jobster adding job container

--- By john Smilga/jobster-app/src/components/Job.js
+++ By john Smilga/jobster-app/src/components/Job.js
@@ -1,6 +1,55 @@
-const Job = () => {
+import { FaLocationArrow, FaBriefcase, FaCalendarAlt } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import Wrapper from "../assets/wrappers/Job";
+import { useDispatch } from "react-redux";
+
+const Job = ({
+  _id,
+  position,
+  company,
+  jobLocation,
+  jobType,
+  createdAt,
+  status,
+}) => {
+  const dispatch = useDispatch();
+
   return (
-    <div>Job</div>
-  )
-}
-export default Job
+    <Wrapper>
+      <header>
+        <div className="main-icon">{company.charAt(0)}</div>
+        <div className="info">
+          <h5>{position}</h5>
+          <p>{company}</p>
+        </div>
+      </header>
+      <div className="content-center">
+        <h4>more content</h4>
+        <div className={`status ${status}`}>{status}</div>
+      </div>
+      <footer>
+        <div className="actions">
+          <Link
+            to="/add-job"
+            className="btn edit-btn"
+            onClick={() => {
+              console.log("edit job");
+            }}
+          >
+            Edit
+          </Link>
+          <button
+            type="button"
+            className="btn delete-btn"
+            onClick={() => {
+              console.log("delete  job");
+            }}
+          >
+            Delete
+          </button>
+        </div>
+      </footer>
+    </Wrapper>
+  );
+};
+export default Job;
