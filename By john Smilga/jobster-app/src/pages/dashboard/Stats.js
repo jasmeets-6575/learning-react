320e305f02f9763aaa67b523870315b03a5e0655
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-05-06 02:33:42 +0530

    jobster adding stats page

--- By john Smilga/jobster-app/src/pages/dashboard/Stats.js
+++ By john Smilga/jobster-app/src/pages/dashboard/Stats.js
@@ -1,6 +1,11 @@
+import { useEffect } from 'react';
+import { StatsContainer, Loading, ChartsContainer } from '../../components';
+import { useDispatch, useSelector } from 'react-redux';
+import { showStats } from '../../features/allJobs/allJobsSlice';
+
 const Stats = () => {
   return (
     <div>Stats</div>
   )
 }
-export default Stats
+export default Stats 
