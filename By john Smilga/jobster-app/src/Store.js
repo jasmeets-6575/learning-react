c8fdbfb78713a11003e57357da89414aa7086e25
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-05-05 17:53:31 +0530

    jobster adding addJob page

--- By john Smilga/jobster-app/src/Store.js
+++ By john Smilga/jobster-app/src/Store.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./features/user/userSlice";
-
+import jobSlice from "./features/job/jobSlice";
 
 export const store = configureStore({
-    reducer:{
-        user:userSlice,
-    },
-})
+  reducer: {
+    user: userSlice,
+    job: jobSlice,
+  },
+});
