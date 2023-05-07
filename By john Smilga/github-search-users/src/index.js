ee6c3f33c762fd29b3b0a083184691ed76c5216b
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-05-07 20:53:17 +0530

    github users adding auth0

--- By john Smilga/github-search-users/src/index.js
+++ By john Smilga/github-search-users/src/index.js
@@ -6,11 +6,19 @@ import * as serviceWorker from "./serviceWorker";
 import { GithubProvider } from "./context/context";
 import { Auth0Provider } from "@auth0/auth0-react";
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
-    <GithubProvider>
-      <App />
-    </GithubProvider>
+    <Auth0Provider
+      domain="jasmeets-6575.us.auth0.com"
+      clientId="Rb5cbrG6TtMs9T2H5NTSjOgPbJaHgF4G"
+      redirectUri={window.location.origin}
+      cacheLocation="localstorage"
+    >
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>
 );
 
