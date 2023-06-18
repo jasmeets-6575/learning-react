7ba0d3d883bd12ab7a178322b27c6820dc64a8f2
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-06-19 03:56:11 +0530

    setting default data

--- By Code with Harry/New folder/news app/src/components/NewsItem.jsx
+++ By Code with Harry/New folder/news app/src/components/NewsItem.jsx
@@ -6,11 +6,32 @@ export default class NewsItem extends Component {
     return (
       <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
-          <img src={url} className="card-img-top" alt="..." />
+          <img
+            src={
+              url
+                ? url
+                : "https://img.etimg.com/thumb/msid-101060778,width-1070,height-580,imgsize-572969,overlay-etwealth/photo.jpg"
+            }
+            className="card-img-top"
+            alt="..."
+          />
           <div className="card-body">
-            <h5 className="card-title">{title ? title.slice(0, 40) : ""}...</h5>
-            <p className="card-text">{desc ? desc.slice(0, 100) : ""}...</p>
-            <a href={id} target="_blank" className="btn btn-sm btn-primary">
+            <h5 className="card-title">
+              {title
+                ? title.slice(0, 40)
+                : "SGB: Sovereign Gold Bond Scheme 2023-24 Series I issue price announced; check details - The Economic Times"}
+              ...
+            </h5>
+            <p className="card-text">{desc ? desc.slice(0, 100) : "The Reserve Bank of India (RBI) has kept the settlement date of this tranche of Sovereign Gold Bond Scheme 2023-24 Series as June 27, 2023."}...</p>
+            <a
+              href={
+                id
+                  ? id
+                  : "https://economictimes.indiatimes.com/wealth/invest/sgb-sovereign-gold-bond-scheme-2023-24-series-i-issue-price-announced-check-details/articleshow/101060746.cms"
+              }
+              target="_blank"
+              className="btn btn-sm btn-primary"
+            >
               Read More...
             </a>
           </div>
