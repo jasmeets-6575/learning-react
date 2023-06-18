807747366753fef1fc7fd162eac520c3567caa61
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-06-19 04:18:43 +0530

    setting pagination

--- By Code with Harry/New folder/news app/src/components/News.jsx
+++ By Code with Harry/New folder/news app/src/components/News.jsx
@@ -7,22 +7,32 @@ export default class News extends Component {
   constructor() {
     super();
     this.state = {
-      articles: this.articles,
+      articles: [],
       loading: false,
+      page: 1,
     };
   }
 
-  async componentDidMount(){
-    let url = "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=cc0605cd3c234fe0b243edde6467acdf"
+  async componentDidMount() {
+    let url =
+      "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=cc0605cd3c234fe0b243edde6467acdf&page=1";
     let data = await fetch(url);
-    let parsedData = await data.json()
-    this.setState({articles: parsedData.articles})
+    let parsedData = await data.json();
+    this.setState({ articles: parsedData.articles });
   }
+  
+  handlePrevClick = () => {
+    console.log("prev");
+  };
+
+  handleNextClick = () => {
+    console.log("next");
+  };
 
   render() {
     return (
       <div className="container my-3">
-        <h2>NewsMonkey - Top Headlines</h2>
+        <h1>NewsMonkey - Top Headlines</h1>
         <div className="row">
           {this.state.articles.map((element) => {
             const { title, description, urlToImage, url } = element;
@@ -37,6 +47,22 @@ export default class News extends Component {
               </div>
             );
           })}
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button
+            type="button"
+            class="btn btn-primary"
+            onClick={this.handlePrevClick}
+          >
+            &larr; Previous
+          </button>
+          <button
+            type="button"
+            class="btn btn-primary"
+            onClick={this.handleNextClick}
+          >
+            Next &rarr;
+          </button>
         </div>
       </div>
     );
