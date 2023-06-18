bbc3a9b3cfea44d84a47c276ed9dd99799ae408b
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-06-18 08:55:39 +0530

    adding News json data

--- By Code with Harry/New folder/news app/src/components/NewsItem.jsx
+++ By Code with Harry/New folder/news app/src/components/NewsItem.jsx
@@ -1,11 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class NewsItem extends Component {
   render() {
     return (
-      <div>
-        This is a news item 
+      <div className="card" style="width: 18rem;">
+        <img src="..." className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="#" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
       </div>
-    )
+    );
   }
 }
