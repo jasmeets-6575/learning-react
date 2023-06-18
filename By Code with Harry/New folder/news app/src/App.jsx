c44daf4a2a2e6e527804ca89dcbd3704046a870d
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-06-18 08:23:48 +0530

    adding News component and newsItem component

--- By Code with Harry/New folder/news app/src/App.jsx
+++ By Code with Harry/New folder/news app/src/App.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import News from "./components/News";
 
 export default class App extends Component {
   render() {
     return <div>
       <Navbar />
+      <News />
     </div>;
   }
 }
