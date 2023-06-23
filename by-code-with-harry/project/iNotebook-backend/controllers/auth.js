c918d310de604319afa62ee6f4ca6eccc1f93678
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-06-23 08:59:15 +0530

    adding get all User

--- by-code-with-harry/project/iNotebook-backend/controllers/auth.js
+++ by-code-with-harry/project/iNotebook-backend/controllers/auth.js
@@ -1,4 +1,9 @@
 const User = require("../models/User");
 const asyncWrapper = require("../asyncWrapper/async");
 
+const getAllUser = asyncWrapper(async (req, res) => {
+  const user = await User.find({});
+  res.status(200).json({ user });
+});
 
+module.exports = { getAllUser };
