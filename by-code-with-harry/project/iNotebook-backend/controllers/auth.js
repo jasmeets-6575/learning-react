c700b71ca82540d52e0fa45a6cf57e5fd49c2864
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-06-23 15:07:40 +0530

    adding login

--- by-code-with-harry/project/iNotebook-backend/controllers/auth.js
+++ by-code-with-harry/project/iNotebook-backend/controllers/auth.js
@@ -3,6 +3,8 @@ const { StatusCodes } = require("http-status-codes");
 const asyncWrapper = require("../asyncWrapper/async");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const BadRequestError = require("../errors/bad-request");
+const UnauthenticatedError = require("../errors/unauthenticated");
 
 const getAllUser = asyncWrapper(async (req, res) => {
   const user = await User.find({});
@@ -26,4 +28,31 @@ const createUser = asyncWrapper(async (req, res) => {
   res.status(StatusCodes.OK).json({ authToken });
 });
 
-module.exports = { getAllUser, createUser };
+const loginUser = asyncWrapper(async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    if (!email || !password) {
+      throw new BadRequestError("Please provide email and password");
+    }
+    let user = await User.findOne({ email });
+    if (!user) {
+      throw new UnauthenticatedError("Invalid Credentials");
+    }
+    const passwordCompare = await bcrypt.compare(password, user.password);
+    if (!passwordCompare) {
+      throw new UnauthenticatedError("Invalid Credentials");
+    }
+    const data = {
+      user: {
+        id: user.id,
+      },
+    };
+    const authToken = jwt.sign(data, process.env.JWT_SECRET);
+    res.status(StatusCodes.OK).json({ authToken });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("internal server Error");
+  }
+});
+
+module.exports = { getAllUser, createUser, loginUser };
--- by-code-with-harry/project/iNotebook-backend/routes/auth.js
+++ by-code-with-harry/project/iNotebook-backend/routes/auth.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { getAllUser, createUser } = require("../controllers/auth");
+const { getAllUser, createUser, loginUser } = require("../controllers/auth");
 
 router.route("/").get(getAllUser);
 router.route("/createuser").post(createUser);
+router.route("/login").post(loginUser);
 
 module.exports = router;
