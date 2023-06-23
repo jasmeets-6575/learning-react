747f2b181a95c0e48f0279b5cd0c4936a83535a8
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-06-23 13:47:37 +0530

    adding hash passwords

--- by-code-with-harry/project/iNotebook-backend/controllers/auth.js
+++ by-code-with-harry/project/iNotebook-backend/controllers/auth.js
@@ -1,5 +1,7 @@
 const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
 const asyncWrapper = require("../asyncWrapper/async");
+const { BadRequestError } = require('../errors')
 const bcrypt = require("bcryptjs");
 
 const getAllUser = asyncWrapper(async (req, res) => {
@@ -8,14 +10,14 @@ const getAllUser = asyncWrapper(async (req, res) => {
 });
 
 const createUser = asyncWrapper(async (req, res) => {
-  const salt = bcrypt.genSalt(10);
+  const salt = await bcrypt.genSalt(10);
   const securedPass = await bcrypt.hash(req.body.password, salt);
   const user = await User.create({
     name: req.body.name,
     password: securedPass,
     email: req.body.email,
   });
-  res.status(201).json({ user });
+  res.status(StatusCodes.OK).json({ user });
 });
 
 module.exports = { getAllUser, createUser };
