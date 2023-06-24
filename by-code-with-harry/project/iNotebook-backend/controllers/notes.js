3ed4d87c1997be34ae4a25d466f2204c97e305ba
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-06-24 08:00:02 +0530

    adding new Notes

--- by-code-with-harry/project/iNotebook-backend/controllers/notes.js
+++ by-code-with-harry/project/iNotebook-backend/controllers/notes.js
@@ -2,8 +2,30 @@ const Notes = require("../models/Notes");
 const { StatusCodes } = require("http-status-codes");
 const asyncWrapper = require("../asyncWrapper/async");
 
-const fetchAllNotes = asyncWrapper(async (req,res)=>{
-  const notes = await Notes.find({user:req.user.id})
-  res.json(notes)
-})
-module.exports = {fetchAllNotes}
+const fetchAllNotes = asyncWrapper(async (req, res) => {
+  try {
+    const notes = await Notes.find({ user: req.user.id });
+    res.json(notes);
+  } catch (error) {
+    console.log(error.message);
+    res.send(error.message);
+  }
+});
+
+const addNote = asyncWrapper(async (req, res) => {
+    console.log(req.user)
+  try {
+    const { title, description, tag } = req.body;
+    const notes = await Notes.create({
+      user: req.user.id,
+      title,
+      description,
+      tag,
+    });
+    res.json(notes);
+  } catch (error) {
+    console.log(error.message);
+    res.send(error.message);
+  }
+});
+module.exports = { fetchAllNotes, addNote };
--- by-code-with-harry/project/iNotebook-backend/routes/notes.js
+++ by-code-with-harry/project/iNotebook-backend/routes/notes.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { fetchAllNotes } = require("../controllers/notes");
+const { fetchAllNotes, addNote } = require("../controllers/notes");
 const router = express.Router();
 const fetchuser = require("../middleware/fetchUser");
 
-router.route("/fetchallnotes").get(fetchuser, fetchAllNotes)
+router.route("/fetchallnotes").get(fetchuser, fetchAllNotes);
+router.route("/addnote").post(fetchuser, addNote);
 
 module.exports = router;
