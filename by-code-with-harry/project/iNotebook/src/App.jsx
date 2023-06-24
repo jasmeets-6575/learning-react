3a5245880e954f8ee1374d5107ca9c00fc452c77
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-06-24 10:41:34 +0530

    setting up navbar

--- by-code-with-harry/project/iNotebook/src/App.jsx
+++ by-code-with-harry/project/iNotebook/src/App.jsx
@@ -1,9 +1,19 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
 
 function App() {
   return (
     <>
-      <div>app</div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </Router>
     </>
   );
 }
