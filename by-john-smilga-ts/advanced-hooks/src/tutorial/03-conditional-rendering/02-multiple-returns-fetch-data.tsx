c580a1a129b6a42a6594ebc3a554018ff4ec818b
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-23 05:16:19 +0530

    multiple conditional example

---
+++ by-john-smilga-ts/advanced-hooks/src/tutorial/03-conditional-rendering/02-multiple-returns-fetch-data.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+const url = "https://api.github.com/users/QuincyLarson";
+
+interface User {
+  avatar_url: string;
+  name: string;
+  company: string;
+  bio: string;
+}
+const MultipleReturnsFetchData = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [isError, setIsError] = useState<boolean>(false);
+  const [user, setUser] = useState<User | null>(null);
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+  if (isError) {
+    return <h2>There was an error...</h2>;
+  }
+  const { avatar_url, name, company, bio } = user as User;
+  return (
+    <div>
+      <img
+        style={{ width: "150px", borderRadius: "25px" }}
+        src={avatar_url}
+        alt={name}
+      />
+      <h2>{name}</h2>
+      <h4>works at {company}</h4>
+      <p>{bio}</p>
+    </div>
+  );
+};
+export default MultipleReturnsFetchData;
