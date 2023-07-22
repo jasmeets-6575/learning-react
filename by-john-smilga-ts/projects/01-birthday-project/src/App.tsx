8f4687b15bc77300579c255f1796b2872a45813e
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-23 02:24:56 +0530

    adding remove button

--- by-john-smilga-ts/projects/01-birthday-project/src/App.tsx
+++ by-john-smilga-ts/projects/01-birthday-project/src/App.tsx
@@ -2,14 +2,24 @@ import { useState } from "react";
 import { data } from "./data";
 import List from "./List";
 import { People } from "./types";
-function App() {
+
+const App = () => {
   const [people, setPeople] = useState<People[]>(data);
   return (
     <main className="container">
-      <h3>{people.length ? people.length : 0} birthdays today</h3>
-      <List people={people} />
+      <section className="container">
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button
+          type="button"
+          className="btn btn-block"
+          onClick={() => setPeople([])}
+        >
+          clear all
+        </button>
+      </section>
     </main>
   );
-}
+};
 
 export default App;
