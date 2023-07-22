c7b9e888cd7a5f7d3385d6fe86898ea07bba2de2
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-23 02:23:01 +0530

    adding person

--- by-john-smilga-ts/projects/01-birthday-project/src/Person.tsx
+++ by-john-smilga-ts/projects/01-birthday-project/src/Person.tsx
@@ -1,4 +1,12 @@
-const Person = () => {
-  return <div>Person</div>;
+const Person = ({ image, name, age }) => {
+  return (
+    <article className="person">
+      <img src={image} alt={name} className="img" />
+      <div>
+        <h4>{name}</h4>
+        <p>{age} years</p>
+      </div>
+    </article>
+  );
 };
 export default Person;
