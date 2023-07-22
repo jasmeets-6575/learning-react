da6e0ad523888735021602c793437595af1785aa
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-22 22:04:45 +0530

    adding data

---
+++ by-john-smilga-ts/advanced-hooks/src/data.tsx
@@ -0,0 +1,34 @@
+interface Person {
+  id: number;
+  name: string;
+  nickName?: string;
+  images?: {
+    small: {
+      url: string;
+    };
+  }[];
+}
+
+export const data: Person[] = [
+  { id: 1, name: "john" },
+  { id: 2, name: "peter" },
+  { id: 3, name: "susan" },
+  { id: 4, name: "anna" },
+];
+
+export const people: Person[] = [
+  { id: 1, name: "bob", nickName: "Stud Muffin" },
+  { id: 2, name: "peter" },
+  {
+    id: 3,
+    name: "oliver",
+    images: [
+      {
+        small: {
+          url: "https://res.cloudinary.com/diqqf3eq2/image/upload/ar_1:1,bo_5px_solid_rgb:ff0000,c_fill,g_auto,r_max,w_1000/v1595959121/person-1_aufeoq.jpg",
+        },
+      },
+    ],
+  },
+  { id: 4, name: "david" },
+];
