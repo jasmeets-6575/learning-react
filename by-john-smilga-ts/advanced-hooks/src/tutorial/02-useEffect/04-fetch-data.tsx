3e2e7804d1fd25a384bad0f3b8a39bff40f161ac
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-23 03:12:53 +0530

    fetching data

--- by-john-smilga-ts/advanced-hooks/src/tutorial/02-useEffect/04-fetch-data.tsx
+++ by-john-smilga-ts/advanced-hooks/src/tutorial/02-useEffect/04-fetch-data.tsx
@@ -1,25 +1,49 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
+
+interface User {
+  id: number;
+  login: string;
+  avatar_url: string;
+  html_url: string;
+}
+
 const url = "https://api.github.com/users";
 
-const FetchData = () => {
-  const [users, setUsers] = useState([]);
+const FetchData: React.FC = () => {
+  const [users, setUsers] = useState<User[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch(url);
-        const users = await response.json();
-        setUsers(users);
+        const usersData = await response.json();
+        setUsers(usersData);
       } catch (error) {
         console.log(error);
       }
     };
     fetchData();
   }, []);
+
   return (
     <section>
-      <h3>{users}</h3>
+      <h3>github users</h3>
+      <ul className="users">
+        {users.map((user) => {
+          const { id, login, avatar_url, html_url } = user;
+          return (
+            <li key={id}>
+              <img src={avatar_url} alt={login} />
+              <div>
+                <h5>{login}</h5>
+                <a href={html_url}>profile</a>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
     </section>
   );
 };
+
 export default FetchData;
