f6959c3cf12a143ae21043f252543918a62595c3
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-22 22:07:03 +0530

    array example

---
+++ by-john-smilga-ts/advanced-hooks/src/tutorial/01-useState/03-useState-array.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { data } from "../../data";
+
+interface Person {
+  id: number;
+  name: string;
+}
+
+const UseStateArray: React.FC = () => {
+  const [people, setPeople] = useState<Person[]>(data);
+
+  const removeItem = (id: number) => {
+    let newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+  };
+
+  return (
+    <div>
+      {people.map((person) => {
+        const { id, name } = person;
+        return (
+          <div key={id} className="item">
+            <h4>{name}</h4>
+            <button onClick={() => removeItem(id)}>remove</button>
+          </div>
+        );
+      })}
+      <button
+        className="btn"
+        style={{ marginTop: "2rem" }}
+        onClick={() => setPeople([])}
+      >
+        clear items
+      </button>
+    </div>
+  );
+};
+
+export default UseStateArray;
