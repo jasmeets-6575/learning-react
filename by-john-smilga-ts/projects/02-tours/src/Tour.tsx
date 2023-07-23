47f94379b7c15af822ce1804795600525f8f95e0
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-23 23:36:30 +0530

    adding read more btn and  functionality

--- by-john-smilga-ts/projects/02-tours/src/Tour.tsx
+++ by-john-smilga-ts/projects/02-tours/src/Tour.tsx
@@ -1,14 +1,24 @@
+import { useState } from "react";
 import { TourType } from "./types.d";
 
 const Tour = ({ id, image, info, name, price, removeTour }: TourType) => {
+  const [readMore, setReadMore] = useState<boolean>(false);
   return (
     <article>
       <img src={image} alt={name} className="img" />
       <span className="tour-price">${price}</span>
       <div className="tour-info">
         <h5>{name}</h5>
-        <p>{info}</p>
-        <button className="delete-btn btn-block" onClick={() => removeTour(id)}>
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+          <button className="info-btn" onClick={() => setReadMore(!readMore)}>
+            {readMore ? "show less" : "  read more"}
+          </button>
+        </p>
+        <button
+          className="delete-btn btn-block btn"
+          onClick={() => removeTour(id)}
+        >
           not interested
         </button>
       </div>
