e630aa62cf49e9cf9e93a11c229c14afd87c7787
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-23 22:57:02 +0530

    fetching tours data

--- by-john-smilga-ts/projects/02-tours/src/App.tsx
+++ by-john-smilga-ts/projects/02-tours/src/App.tsx
@@ -1,4 +1,35 @@
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
+
+const url = "https://course-api.com/react-tours-project";
+
 function App() {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchTours();
+  }, []);
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
   return <h1>Hello World</h1>;
 }
 
