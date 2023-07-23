5f9616312969b1f12e8474269ba08a040d7d35c3
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-24 04:23:27 +0530

    adding categories

--- by-john-smilga-ts/projects/05-menu/src/App.tsx
+++ by-john-smilga-ts/projects/05-menu/src/App.tsx
@@ -3,13 +3,27 @@ import Title from "./Title";
 import items from "./data";
 import { MenuItem } from "./types.d";
 import Menu from "./Menu";
+import Categories from "./Categories";
 
+const allCategories = ["all", ...new Set(items.map((item) => item.category))];
 function App() {
   const [menuItems, setMenuItems] = useState<MenuItem[]>(items);
+  const [categories, setCategories] = useState<string[]>(allCategories);
+
+  const filterItems = (category: string) => {
+    if (category === "all") {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
   return (
     <main>
       <section className="menu">
         <Title text={"Our Menu"} />
+        <Categories categories={categories} filterItems={filterItems} />
         <Menu items={menuItems} />
       </section>
     </main>
--- by-john-smilga-ts/projects/05-menu/src/Categories.tsx
+++ by-john-smilga-ts/projects/05-menu/src/Categories.tsx
@@ -1,4 +1,27 @@
-const Categories = () => {
-  return <div>Categories</div>;
+import React from "react";
+
+interface CategoriesProps {
+  categories: string[];
+  filterItems: (category: string) => void;
+}
+
+const Categories: React.FC<CategoriesProps> = ({ categories, filterItems }) => {
+  return (
+    <div className="btn-container">
+      {categories.map((category) => {
+        return (
+          <button
+            type="button"
+            className="btn"
+            key={category}
+            onClick={() => filterItems(category)}
+          >
+            {category}
+          </button>
+        );
+      })}
+    </div>
+  );
 };
+
 export default Categories;
