68345c0852c1791be6c4361f94b1649510244562
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-23 06:00:21 +0530

    user challenge complete

--- by-john-smilga-ts/advanced-hooks/src/tutorial/03-conditional-rendering/07-user-challenge.tsx
+++ by-john-smilga-ts/advanced-hooks/src/tutorial/03-conditional-rendering/07-user-challenge.tsx
@@ -16,7 +16,25 @@ const UserChallenge: React.FC = () => {
     setUser(null);
   };
 
-  return <div>hello world</div>;
+  return (
+    <div>
+      {user ? (
+        <div>
+          <h4>hello there, {user.name}</h4>
+          <button className="btn" onClick={logout}>
+            logout
+          </button>
+        </div>
+      ) : (
+        <div>
+          <h4>Please Login</h4>
+          <button className="btn" onClick={login}>
+            login
+          </button>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default UserChallenge;
