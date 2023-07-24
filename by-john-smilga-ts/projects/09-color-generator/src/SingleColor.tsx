d415a6ef81eb0967a1c31facfc133a15994a088f
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-25 05:13:19 +0530

    singelColor

--- by-john-smilga-ts/projects/09-color-generator/src/SingleColor.tsx
+++ by-john-smilga-ts/projects/09-color-generator/src/SingleColor.tsx
@@ -12,6 +12,14 @@ interface SingleColorProps {
 }
 const SingleColor: React.FC<SingleColorProps> = ({ index, color }) => {
   const { hex, weight } = color;
-  return <div>SingleColor</div>;
+  return (
+    <article
+      className={index > 10 ? "color color-light" : "color"}
+      style={{ background: `#${hex}` }}
+    >
+      <p className="percent-value">{weight}%</p>
+      <p className="color-value">#{hex}</p>
+    </article>
+  );
 };
 export default SingleColor;
