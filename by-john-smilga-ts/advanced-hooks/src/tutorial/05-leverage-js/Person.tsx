a8ce8bfeb987b10762f8e7e5ac52de378546df02
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-24 23:45:50 +0530

    leverage perosn

--- by-john-smilga-ts/advanced-hooks/src/tutorial/05-leverage-js/Person.tsx
+++ by-john-smilga-ts/advanced-hooks/src/tutorial/05-leverage-js/Person.tsx
@@ -1,4 +1,32 @@
-const Person = () => {
-  return <div>Person</div>;
+import React from "react";
+import avatar from "../../../assets/default-avatar.svg";
+interface Image {
+  small: {
+    url: string;
+  };
+}
+interface PersonProps {
+  name: string;
+  nickName?: string;
+  images?: Image[];
+}
+
+const Person: React.FC<PersonProps> = ({
+  name,
+  nickName = "shakeAndBake",
+  images,
+}) => {
+  // before optional chaining
+
+  // const img =
+  //   (images && images[0] && images[0].small && images[0].small.url) || avatar;
+  const img = images?.[0]?.small?.url || avatar;
+  return (
+    <div>
+      <img src={img} alt={name} style={{ width: "50px" }} />
+      <h4>{name} </h4>
+      <p>Nickname : {nickName}</p>
+    </div>
+  );
 };
 export default Person;
