77b7ef292662c126a75bd25c4a6f5d1c7fb92cf6
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-25 04:34:48 +0530

    lorem ipsum complete

--- by-john-smilga-ts/projects/08-lorem-ipsum/src/App.tsx
+++ by-john-smilga-ts/projects/08-lorem-ipsum/src/App.tsx
@@ -1,5 +1,43 @@
-function App() {
-  return <h1>Hello</h1>;
-}
+import React, { useState, FormEvent } from "react";
+import data from "./data";
+import { nanoid } from "nanoid";
+
+const App: React.FC = () => {
+  const [count, setCount] = useState<number>(1);
+  const [text, setText] = useState<string[]>([]);
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    let amount = parseInt(count.toString(), 10);
+    setText(data.slice(0, amount));
+  };
+
+  return (
+    <section className="section-center">
+      <h4>tired of boring lorem ipsum?</h4>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          min="1"
+          step="1"
+          max="8"
+          value={count}
+          onChange={(e) => setCount(parseInt(e.target.value, 10))}
+        />
+        <button className="btn" type="submit">
+          generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        {text.map((item) => {
+          return <p key={nanoid()}>{item}</p>;
+        })}
+      </article>
+    </section>
+  );
+};
 
 export default App;
