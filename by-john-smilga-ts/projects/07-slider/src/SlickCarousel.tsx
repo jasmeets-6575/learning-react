778f3178b68843bccc25f2c42f61a3c8900298b4
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-24 23:09:15 +0530

    Slick Carousel complete

--- by-john-smilga-ts/projects/07-slider/src/SlickCarousel.tsx
+++ by-john-smilga-ts/projects/07-slider/src/SlickCarousel.tsx
@@ -6,6 +6,34 @@ import { list } from "./data";
 import { FaQuoteRight } from "react-icons/fa";
 
 const SlickCarousel = () => {
-  return <div>SlickCarousel</div>;
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    // fade: true,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    pauseOnHover: true,
+  };
+  return (
+    <section className="slick-container">
+      <Slider {...settings}>
+        {list.map((person) => {
+          const { id, image, name, title, quote } = person;
+          return (
+            <article key={id}>
+              <img src={image} alt={name} className="person-img" />
+              <h5 className="name">{name}</h5>
+              <p className="title">{title}</p>
+              <p className="text">{quote}</p>
+              <FaQuoteRight className="icon" />
+            </article>
+          );
+        })}
+      </Slider>
+    </section>
+  );
 };
 export default SlickCarousel;
