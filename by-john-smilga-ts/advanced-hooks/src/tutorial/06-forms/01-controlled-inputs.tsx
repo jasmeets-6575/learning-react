ae7229ebf4d9082f47644b8c3576ca1f58f16e81
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-25 02:09:36 +0530

    controlled inputs

---
+++ by-john-smilga-ts/advanced-hooks/src/tutorial/06-forms/01-controlled-inputs.tsx
@@ -0,0 +1,48 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+const ControlledInputs = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // do something
+    console.log(name, email);
+  };
+  return (
+    <form className="form" onSubmit={handleSubmit}>
+      <h4>controlled inputs</h4>
+      <div className="form-row">
+        <label htmlFor="name" className="form-label">
+          name
+        </label>
+        <input
+          type="text"
+          className="form-input"
+          value={name}
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            setName(e.target.value)
+          }
+          id="name"
+        />
+      </div>
+      <div className="form-row">
+        <label htmlFor="email" className="form-label">
+          Email
+        </label>
+        <input
+          type="email"
+          className="form-input"
+          id="email"
+          value={email}
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            setEmail(e.target.value)
+          }
+        />
+      </div>
+      <button type="submit" className="btn btn-block">
+        submit
+      </button>
+    </form>
+  );
+};
+export default ControlledInputs;
