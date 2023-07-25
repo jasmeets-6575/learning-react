0658c12401a03e1c24af4b227f926802cf79c7e2
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-25 22:35:28 +0530

    adding Form

--- by-john-smilga-ts/projects/10-grocery-bud/src/Form.tsx
+++ by-john-smilga-ts/projects/10-grocery-bud/src/Form.tsx
@@ -1,4 +1,34 @@
-const Form = () => {
-  return <div>Form</div>;
+import React, { useState } from "react";
+
+interface FormProps {
+  addItem: (itemName: string) => void;
+}
+
+const Form: React.FC<FormProps> = ({ addItem }) => {
+  const [newItemName, setNewItemName] = useState<string>("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    addItem(newItemName);
+    setNewItemName("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h4>grocery bud</h4>
+      <div className="form-control">
+        <input
+          type="text"
+          className="form-input"
+          value={newItemName}
+          onChange={(event) => setNewItemName(event.target.value)}
+        />
+        <button type="submit" className="btn">
+          add item
+        </button>
+      </div>
+    </form>
+  );
 };
+
 export default Form;
