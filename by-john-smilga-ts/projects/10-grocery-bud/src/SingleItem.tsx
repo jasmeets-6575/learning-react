92d98a886d6bea379c4bef00c911d58251e7de34
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-25 22:31:07 +0530

    Single Item

--- by-john-smilga-ts/projects/10-grocery-bud/src/SingleItem.tsx
+++ by-john-smilga-ts/projects/10-grocery-bud/src/SingleItem.tsx
@@ -1,4 +1,46 @@
-const SingleItem = () => {
-  return <div>SingleItem</div>;
+import React from "react";
+
+interface Item {
+  id: number;
+  name: string;
+  completed: boolean;
+}
+
+interface SingleItemProps {
+  item: Item;
+  removeItem: (id: number) => void;
+  editItem: (id: number) => void;
+}
+
+const SingleItem: React.FC<SingleItemProps> = ({
+  item,
+  removeItem,
+  editItem,
+}) => {
+  return (
+    <div className="single-item">
+      <input
+        type="checkbox"
+        checked={item.completed}
+        onChange={() => editItem(item.id)}
+      />
+      <p
+        style={{
+          textTransform: "capitalize",
+          textDecoration: item.completed ? "line-through" : "none",
+        }}
+      >
+        {item.name}
+      </p>
+      <button
+        className="btn remove-btn"
+        type="button"
+        onClick={() => removeItem(item.id)}
+      >
+        delete
+      </button>
+    </div>
+  );
 };
+
 export default SingleItem;
