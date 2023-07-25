7159317ac4fa9b0ce935c5faf7fa113443fda5fb
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-25 22:40:07 +0530

    adding local storage

--- by-john-smilga-ts/projects/10-grocery-bud/src/App.tsx
+++ by-john-smilga-ts/projects/10-grocery-bud/src/App.tsx
@@ -4,38 +4,57 @@ import { nanoid } from "nanoid";
 import Items from "./Items";
 import { ToastContainer, toast } from "react-toastify";
 
-interface Item {
+interface TodoItem {
   name: string;
   completed: boolean;
   id: string;
 }
 
+const getLocalStorage = (): TodoItem[] => {
+  let list = localStorage.getItem("list");
+  if (list) {
+    return JSON.parse(list);
+  } else {
+    return [];
+  }
+};
+
+const setLocalStorage = (items: TodoItem[]): void => {
+  localStorage.setItem("list", JSON.stringify(items));
+};
+
 const App: React.FC = () => {
+  const [items, setItems] = useState<TodoItem[]>(getLocalStorage);
+
   const addItem = (itemName: string): void => {
-    const newItem: Item = {
+    const newItem: TodoItem = {
       name: itemName,
       completed: false,
       id: nanoid(),
     };
-    const newItems: Item[] = [...items, newItem];
+    const newItems: TodoItem[] = [...items, newItem];
+    setItems(newItems);
+    setLocalStorage(newItems);
     toast.success("item added to the list");
   };
 
   const removeItem = (itemId: string): void => {
-    const newItems: Item[] = items.filter((item) => item.id !== itemId);
+    const newItems: TodoItem[] = items.filter((item) => item.id !== itemId);
     setItems(newItems);
+    setLocalStorage(newItems);
     toast.success("item deleted");
   };
 
   const editItem = (itemId: string): void => {
-    const newItems: Item[] = items.map((item) => {
+    const newItems: TodoItem[] = items.map((item) => {
       if (item.id === itemId) {
-        const newItem: Item = { ...item, completed: !item.completed };
+        const newItem: TodoItem = { ...item, completed: !item.completed };
         return newItem;
       }
       return item;
     });
     setItems(newItems);
+    setLocalStorage(newItems);
   };
 
   useEffect(() => {
