f88683e1169ffacd4f041d6f13b670bd3b4165ee
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-25 22:32:24 +0530

    Items

--- by-john-smilga-ts/projects/10-grocery-bud/src/Items.tsx
+++ by-john-smilga-ts/projects/10-grocery-bud/src/Items.tsx
@@ -1,4 +1,33 @@
-const Items = () => {
-  return <div>Items</div>;
+import React from "react";
+import SingleItem from "./SingleItem";
+
+interface Item {
+  id: number;
+  name: string;
+  completed: boolean;
+}
+
+interface ItemsProps {
+  items: Item[];
+  removeItem: (id: number) => void;
+  editItem: (id: number) => void;
+}
+
+const Items: React.FC<ItemsProps> = ({ items, removeItem, editItem }) => {
+  return (
+    <div className="items">
+      {items.map((item) => {
+        return (
+          <SingleItem
+            key={item.id}
+            item={item}
+            removeItem={removeItem}
+            editItem={editItem}
+          />
+        );
+      })}
+    </div>
+  );
 };
+
 export default Items;
