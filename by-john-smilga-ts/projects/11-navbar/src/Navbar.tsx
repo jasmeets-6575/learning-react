996a382b455c1b8b1b6cfd2f93e0f810ee092d31
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-26 23:39:42 +0530

    structure

--- by-john-smilga-ts/projects/11-navbar/src/Navbar.tsx
+++ by-john-smilga-ts/projects/11-navbar/src/Navbar.tsx
@@ -1,4 +1,45 @@
+import { useState, useRef } from "react";
+import { FaBars } from "react-icons/fa";
+import { links, social } from "./data";
+import logo from "./logo.svg";
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [showLinks, setShowLinks] = useState(false);
+
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="logo" alt="logo" />
+          <button className="nav-toggle">
+            <FaBars />
+          </button>
+        </div>
+
+        <div className="links-container">
+          <ul className="links">
+            {links.map((link) => {
+              const { id, url, text } = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        {/* social links */}
+        <ul className="social-icons">
+          {social.map((socialIcon) => {
+            const { id, url, icon } = socialIcon;
+            return (
+              <li key={id}>
+                <a href={url}>{icon}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
 };
 export default Navbar;
