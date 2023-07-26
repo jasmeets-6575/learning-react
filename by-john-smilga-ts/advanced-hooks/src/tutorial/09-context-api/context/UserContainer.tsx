4fb7c5da1cd4997fcbd451bb01f174960955c679
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-27 03:44:05 +0530

    usercontainer

---
+++ by-john-smilga-ts/advanced-hooks/src/tutorial/09-context-api/context/UserContainer.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useAppContext } from "./Navbar";
+
+const UserContainer: React.FC = () => {
+  const { user, logout } = useAppContext();
+
+  return (
+    <div className="user-container">
+      {user ? (
+        <>
+          <p>Hello There, {user.name.toUpperCase()}</p>
+          <button type="button" className="btn" onClick={logout}>
+            logout
+          </button>
+        </>
+      ) : (
+        <p>Please Login</p>
+      )}
+    </div>
+  );
+};
+
+export default UserContainer;
