e4171d61b2dfe7e099c08712317ad665ce6281ff
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-27 05:20:11 +0530

    Home component

--- by-john-smilga-ts/projects/12-sidebar/src/Home.tsx
+++ by-john-smilga-ts/projects/12-sidebar/src/Home.tsx
@@ -1,4 +1,20 @@
-const Home = () => {
-  return <div>Home</div>;
+import React from "react";
+import { useGlobalContext } from "./Context";
+import { FaBars } from "react-icons/fa";
+
+const Home: React.FC = () => {
+  const { openSidebar, openModal } = useGlobalContext();
+
+  return (
+    <main>
+      <button onClick={openSidebar} className="sidebar-toggle">
+        <FaBars />
+      </button>
+      <button onClick={openModal} className="btn">
+        show modal
+      </button>
+    </main>
+  );
 };
+
 export default Home;
