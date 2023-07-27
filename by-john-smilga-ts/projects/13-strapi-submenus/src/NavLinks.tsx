fdab14d07cc80bc6ea8132d5e05b6ff717ef56de
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-28 02:49:02 +0530

    adding submenu

--- by-john-smilga-ts/projects/13-strapi-submenus/src/NavLinks.tsx
+++ by-john-smilga-ts/projects/13-strapi-submenus/src/NavLinks.tsx
@@ -2,12 +2,17 @@ import { useGlobalContext } from "./Context";
 import sublinks from "./data";
 
 const NavLinks = () => {
+  const { setPageId } = useGlobalContext();
   return (
     <div className="nav-links">
       {sublinks.map((item) => {
         const { pageId, page } = item;
         return (
-          <button key={pageId} className="nav-link">
+          <button
+            key={pageId}
+            className="nav-link"
+            onMouseEnter={() => setPageId(pageId)}
+          >
             {page}
           </button>
         );
--- by-john-smilga-ts/projects/13-strapi-submenus/src/Submenu.tsx
+++ by-john-smilga-ts/projects/13-strapi-submenus/src/Submenu.tsx
@@ -1,4 +1,32 @@
+import { useGlobalContext } from "./Context";
+import sublinks from "./data";
+import { useRef } from "react";
+
 const Submenu = () => {
-  return <div>Submenu</div>;
+  const { pageId, setPageId } = useGlobalContext();
+  const currentPage = sublinks.find((item) => item.pageId === pageId);
+
+  return (
+    <div className={currentPage ? "submenu show-submenu" : "submenu"}>
+      <h5>{currentPage?.page}</h5>
+      <div
+        className="submenu-links"
+        style={{
+          gridTemplateColumns:
+            currentPage?.links?.length ?? 0 > 3 ? "1fr 1fr" : "1fr",
+        }}
+      >
+        {currentPage?.links?.map((link) => {
+          const { id, url, label, icon } = link;
+          return (
+            <a key={id} href={url}>
+              {icon}
+              {label}
+            </a>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 export default Submenu;
