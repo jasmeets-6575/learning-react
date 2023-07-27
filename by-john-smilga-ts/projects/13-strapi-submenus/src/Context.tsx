c43b58b4c9e84c932459314dd1dbc4441b3e7d7e
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-28 01:45:23 +0530

    setting up Context

---
+++ by-john-smilga-ts/projects/13-strapi-submenus/src/Context.tsx
@@ -0,0 +1,48 @@
+import {
+  createContext,
+  useState,
+  useContext,
+  ReactElement,
+  Dispatch,
+  SetStateAction,
+} from "react";
+
+interface AppContextValueType {
+  isSidebarOpen: boolean;
+  openSidebar: () => void;
+  closeSidebar: () => void;
+  pageId: number | null;
+  setPageId: Dispatch<SetStateAction<null>>;
+}
+const AppContext = createContext<AppContextValueType>(
+  {} as AppContextValueType
+);
+
+type ChildrenProps = { children: ReactElement | ReactElement[] };
+export const AppProvider = ({ children }: ChildrenProps): ReactElement => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [pageId, setPageId] = useState(null);
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+  const AppContextValue: AppContextValueType = {
+    isSidebarOpen,
+    openSidebar,
+    closeSidebar,
+    pageId,
+    setPageId,
+  };
+
+  return (
+    <AppContext.Provider value={AppContextValue}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = (): AppContextValueType => {
+  return useContext(AppContext);
+};
