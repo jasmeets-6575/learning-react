ae146bc84fdd66409f0d7a2ca7976b489825989d
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-28 02:22:54 +0530

    navlink

--- by-john-smilga-ts/projects/13-strapi-submenus/src/NavLinks.tsx
+++ by-john-smilga-ts/projects/13-strapi-submenus/src/NavLinks.tsx
@@ -1,4 +1,18 @@
+import { useGlobalContext } from "./Context";
+import sublinks from "./data";
+
 const NavLinks = () => {
-  return <div>NavLinks</div>;
+  return (
+    <div className="nav-links">
+      {sublinks.map((item) => {
+        const { pageId, page } = item;
+        return (
+          <button key={pageId} className="nav-link">
+            {page}
+          </button>
+        );
+      })}
+    </div>
+  );
 };
 export default NavLinks;
