75581d44595f9fc1c1b013bb8ac94af3e7472e7d
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-28 23:07:14 +0530

    setting up Actions types and init state

--- by-john-smilga-ts/projects/14-cart/src/context.tsx
+++ by-john-smilga-ts/projects/14-cart/src/context.tsx
@@ -1,11 +1,4 @@
-import {
-  useContext,
-  useReducer,
-  useEffect,
-  createContext,
-  ReactElement,
-} from "react";
-import reducer from "./reducer";
+import { useContext, useReducer, createContext, ReactElement } from "react";
 
 export type CartitemsType = {
   id: string;
@@ -14,17 +7,41 @@ export type CartitemsType = {
   img: string;
   amount: number;
 };
-type CartStateType = { cart: [] };
-
-const initCartState: CartStateType = { cart: [] };
 
 const url = "https://www.course-api.com/react-useReducer-cart-project";
-
 const AppContext = createContext<CartitemsType>({} as CartitemsType);
+
+type CartStateType = { cart: []; loading: boolean };
+const initCartState: CartStateType = { cart: [], loading: false };
+
+const REDUCER_ACTION_TYPE = {
+  CLEAR_CART: "CLEAR_CART",
+  REMOVE: "REMOVE",
+  INCREASE: "INCREASE",
+  DECREASE: "DECREASE",
+  LOADING: "LOADING",
+  DISPLAY_ITEMS: "DISPLAY_ITEMS",
+};
+
+export type ReducerActionType = typeof REDUCER_ACTION_TYPE;
+export type ReducerAction = {
+  type: string;
+  payload?: CartitemsType;
+};
+
+const reducer = (
+  state: CartStateType,
+  action: ReducerAction
+): CartStateType => {
+  console.log(action);
+
+  return state;
+};
 
 type ChildrenType = { children?: ReactElement | ReactElement[] };
 
 export const CartProvider = ({ children }: ChildrenType): ReactElement => {
+  const [state, dispatch] = useReducer(reducer, initCartState);
   const AppContextValue = {};
   return (
     <AppContext.Provider value={AppContextValue}>
