24a04432b43b1642b3c4408f6fe1d818461b288a
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-29 05:39:49 +0530

    slowfunction

---
+++ by-john-smilga-ts/advanced-hooks/src/tutorial/11-performance/03-hooks/index.tsx
@@ -0,0 +1,35 @@
+import { useState, useCallback, useMemo } from "react";
+import { data } from "../../../data";
+import List from "./List";
+import slowFunction from "./slowFunction";
+
+const LowerState = () => {
+  const [people, setPeople] = useState(data);
+  const [count, setCount] = useState<number>(0);
+
+  const value = useMemo(() => slowFunction(), []);
+  console.log(value);
+
+  const removePerson = useCallback(
+    (id: number) => {
+      console.log(people);
+      const newPeople = people.filter((person) => person.id !== id);
+      setPeople(newPeople);
+    },
+    [people]
+  );
+
+  return (
+    <section>
+      <button
+        className="btn"
+        onClick={() => setCount(count + 1)}
+        style={{ marginBottom: "1rem" }}
+      >
+        count {count}
+      </button>
+      <List people={people} removePerson={removePerson} />
+    </section>
+  );
+};
+export default LowerState;
