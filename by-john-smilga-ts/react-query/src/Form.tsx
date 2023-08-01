7f8de2c483fb77472a7077ec73173768e51da511
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-08-01 10:10:05 +0530

    adding form

--- by-john-smilga-ts/react-query/src/Form.tsx
+++ by-john-smilga-ts/react-query/src/Form.tsx
@@ -1,4 +1,29 @@
-const Form = () => {
-  return <div>Form</div>;
+import { useState, FormEvent } from "react";
+
+const Form: React.FC = () => {
+  const [newItemName, setNewItemName] = useState<string>(""); // Add type annotation for the state
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // Handle the form submission logic here
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h4>task bud</h4>
+      <div className="form-control">
+        <input
+          type="text"
+          className="form-input"
+          value={newItemName}
+          onChange={(event) => setNewItemName(event.target.value)}
+        />
+        <button type="submit" className="btn">
+          add task
+        </button>
+      </div>
+    </form>
+  );
 };
+
 export default Form;
