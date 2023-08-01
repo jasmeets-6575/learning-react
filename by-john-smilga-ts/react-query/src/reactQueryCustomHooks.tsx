da6b7dc760d11c2143baf9c02ac54a0afcc91004
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-08-01 11:35:21 +0530

    fetch tasks

--- by-john-smilga-ts/react-query/src/reactQueryCustomHooks.tsx
+++ by-john-smilga-ts/react-query/src/reactQueryCustomHooks.tsx
@@ -1,4 +1,21 @@
-const reactQueryCustomHooks = () => {
-  return <div>reactQueryCustomHooks</div>;
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import customFetch from "./utils";
+import { toast } from "react-toastify";
+
+interface Task {
+  id: number;
+  title: string;
+  isDone: boolean;
+}
+
+// useFetchTasks hook
+export const useFetchTasks = () => {
+  const { isLoading, data, isError, error } = useQuery<Task[], Error>({
+    queryKey: ["tasks"],
+    queryFn: async () => {
+      const { data } = await customFetch.get<Task[]>("/");
+      return data;
+    },
+  });
+  return { isLoading, isError, data };
 };
-export default reactQueryCustomHooks;
