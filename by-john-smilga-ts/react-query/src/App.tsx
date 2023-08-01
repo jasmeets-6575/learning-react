315e8384ae2211bf4528084304a23fd24f9c10f9
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-08-01 10:09:58 +0530

    adding toastify

--- by-john-smilga-ts/react-query/src/App.tsx
+++ by-john-smilga-ts/react-query/src/App.tsx
@@ -1,5 +1,31 @@
-function App() {
-  return <h1>Hello world</h1>;
+import { ToastContainer } from "react-toastify";
+import { nanoid } from "nanoid";
+import Form from "./Form";
+import Items from "./Items";
+import { useState } from "react";
+
+interface Item {
+  id: string;
+  title: string;
+  isDone: boolean;
 }
+
+const defaultItems: Item[] = [
+  { id: nanoid(), title: "walk the dog", isDone: false },
+  { id: nanoid(), title: "wash dishes", isDone: false },
+  { id: nanoid(), title: "drink coffee", isDone: true },
+  { id: nanoid(), title: "take a nap", isDone: false },
+];
+
+const App: React.FC = () => {
+  const [items, setItems] = useState<Item[]>(defaultItems); // Add type annotation for the 'items' state
+  return (
+    <section className="section-center">
+      <ToastContainer position="top-center" />
+      <Form />
+      <Items items={items} />
+    </section>
+  );
+};
 
 export default App;
