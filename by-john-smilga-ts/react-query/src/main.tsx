a565b9c4d30315d85ed86a025b45c28dd6181cf8
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-08-01 11:06:58 +0530

    query client

--- by-john-smilga-ts/react-query/src/main.tsx
+++ by-john-smilga-ts/react-query/src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import "react-toastify/dist/ReactToastify.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <QueryClientProvider client={queryClient}>
     <App />
-  </React.StrictMode>,
-)
+  </QueryClientProvider>
+);
