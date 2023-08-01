174f0285102769ee68a74ba1630ef9b23c8c60c8
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-08-01 22:27:50 +0530

    gallery

--- by-john-smilga-ts/unsplash-images-project/src/Gallery.tsx
+++ by-john-smilga-ts/unsplash-images-project/src/Gallery.tsx
@@ -1,4 +1,41 @@
-const Gallery = () => {
-  return <div>Gallery</div>;
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { useGlobalContext } from "./context";
+
+const url = `https://api.unsplash.com/search/photos?client_id=${
+  import.meta.env.VITE_API_KEY
+}`;
+
+type ImageData = {
+  id: string;
+  urls: {
+    regular: string;
+  };
+  alt_description: string;
 };
+
+type ApiResponse = {
+  results: ImageData[];
+};
+
+const Gallery = () => {
+  const { searchTerm } = useGlobalContext();
+
+  return (
+    <section className="image-container">
+      {results.map((item) => {
+        const url = item?.urls?.regular;
+        return (
+          <img
+            src={url}
+            key={item.id}
+            alt={item.alt_description}
+            className="img"
+          ></img>
+        );
+      })}
+    </section>
+  );
+};
+
 export default Gallery;
