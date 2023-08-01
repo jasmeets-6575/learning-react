58e4df72ad7dabd396f9fd7b14338d800c73a82b
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-08-01 10:10:13 +0530

    items

--- by-john-smilga-ts/react-query/src/Items.tsx
+++ by-john-smilga-ts/react-query/src/Items.tsx
@@ -1,4 +1,24 @@
-const Items = () => {
-  return <div>Items</div>;
+import React from "react";
+import SingleItem from "./SingleItem";
+
+interface Item {
+  id: string;
+  title: string;
+  isDone: boolean;
+}
+
+interface ItemsProps {
+  items: Item[];
+}
+
+const Items: React.FC<ItemsProps> = ({ items }) => {
+  return (
+    <div className="items">
+      {items.map((item) => {
+        return <SingleItem key={item.id} item={item} />;
+      })}
+    </div>
+  );
 };
+
 export default Items;
--- by-john-smilga-ts/react-query/src/SingleItem.tsx
+++ by-john-smilga-ts/react-query/src/SingleItem.tsx
@@ -1,4 +1,40 @@
-const SingleItem = () => {
-  return <div>SingleItem</div>;
+import React from "react";
+
+interface Item {
+  id: string;
+  title: string;
+  isDone: boolean;
+}
+
+interface SingleItemProps {
+  item: Item;
+}
+
+const SingleItem: React.FC<SingleItemProps> = ({ item }) => {
+  return (
+    <div className="single-item">
+      <input
+        type="checkbox"
+        checked={item.isDone}
+        onChange={() => console.log("edit task")}
+      />
+      <p
+        style={{
+          textTransform: "capitalize",
+          textDecoration: item.isDone ? "line-through" : "none", // Adjusted to set "line-through" when 'isDone' is true, otherwise "none"
+        }}
+      >
+        {item.title}
+      </p>
+      <button
+        className="btn remove-btn"
+        type="button"
+        onClick={() => console.log("delete task")}
+      >
+        delete
+      </button>
+    </div>
+  );
 };
+
 export default SingleItem;
