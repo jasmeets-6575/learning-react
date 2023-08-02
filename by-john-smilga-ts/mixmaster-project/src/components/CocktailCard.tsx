a6efb712da80b04fcd34b1f4f6430ad25ec60d0b
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-08-02 08:24:38 +0530

    cocktailCard

--- by-john-smilga-ts/mixmaster-project/src/components/CocktailCard.tsx
+++ by-john-smilga-ts/mixmaster-project/src/components/CocktailCard.tsx
@@ -1,4 +1,29 @@
-const CocktailCard = () => {
-  return <div>CocktailCard</div>;
+import { Link } from "react-router-dom";
+import Wrapper from "../assets/wrappers/CocktailCard";
+
+type DrinkType = {
+  id: number;
+  name: string;
+  image: string;
+  info: string;
+  glass: string;
+};
+
+const CocktailCard = ({ image, name, id, info, glass }: DrinkType) => {
+  return (
+    <Wrapper>
+      <div className="img-container">
+        <img src={image} alt={name} className="img" />
+      </div>
+      <div className="footer">
+        <h4>{name}</h4>
+        <h5>{glass}</h5>
+        <p>{info}</p>
+        <Link to={`/cocktail/${id}`} className="btn">
+          details
+        </Link>
+      </div>
+    </Wrapper>
+  );
 };
 export default CocktailCard;
