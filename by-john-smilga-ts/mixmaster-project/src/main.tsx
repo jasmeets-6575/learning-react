af42e075e54c505df7dc53d533cd521c52be54ee
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-08-02 09:47:05 +0530

    toastify

--- by-john-smilga-ts/mixmaster-project/src/main.tsx
+++ by-john-smilga-ts/mixmaster-project/src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <ToastContainer position="top-center" autoClose={2000} />
+
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
