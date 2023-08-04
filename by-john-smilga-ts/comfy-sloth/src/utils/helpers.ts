a9d5fed771f6c69fbf9a1388e38316e53546e60f
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-08-04 21:39:03 +0530

    product

--- by-john-smilga-ts/comfy-sloth/src/utils/helpers.ts
+++ by-john-smilga-ts/comfy-sloth/src/utils/helpers.ts
@@ -1,11 +1,14 @@
-export const formatPrice = (number) => {
+export const formatPrice = (number: number) => {
   return new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
   }).format(number / 100);
 };
 
-export const getUniqueValues = (data, type) => {
+export const getUniqueValues = (
+  data: any[],
+  type: string
+): (string | "all")[] => {
   let unique = data.map((item) => item[type]);
   if (type === "colors") {
     unique = unique.flat();
