fb9d806810dbb05c4aa99bce25b98f91f59b576a
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-08-04 05:45:32 +0530

    checkout page

--- by-john-smilga-ts/comfy-sloth/src/pages/CheckoutPage.tsx
+++ by-john-smilga-ts/comfy-sloth/src/pages/CheckoutPage.tsx
@@ -1,12 +1,25 @@
-import React from 'react'
-import styled from 'styled-components'
-import { PageHero, StripeCheckout } from '../components'
+import React from "react";
+import styled from "styled-components";
+import { PageHero, StripeCheckout } from "../components";
 // extra imports
-import { useCartContext } from '../context/cart_context'
-import { Link } from 'react-router-dom'
+import { useCartContext } from "../context/cart_context";
+import { Link } from "react-router-dom";
 
 const CheckoutPage = () => {
-  return <h4>checkout page</h4>
-}
-const Wrapper = styled.div``
-export default CheckoutPage
+  return (
+    <main>
+      <PageHero title="checkout" />
+      <Wrapper className="page">
+        <div className="empty">
+          <h2>Your cart is empty</h2>
+          <Link to="/products" className="btn">
+            fill it
+          </Link>
+        </div>
+        <StripeCheckout />
+      </Wrapper>
+    </main>
+  );
+};
+const Wrapper = styled.div``;
+export default CheckoutPage;
