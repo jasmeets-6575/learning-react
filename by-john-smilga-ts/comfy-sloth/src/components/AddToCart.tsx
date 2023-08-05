7601ab62998179a3e24282997c4e326910ed3d77
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-08-05 20:09:41 +0530

    add to cart

--- by-john-smilga-ts/comfy-sloth/src/components/AddToCart.tsx
+++ by-john-smilga-ts/comfy-sloth/src/components/AddToCart.tsx
@@ -4,9 +4,40 @@ import { Link } from "react-router-dom";
 import { FaCheck } from "react-icons/fa";
 import { useCartContext } from "../context/cart_context";
 import AmountButtons from "./AmountButtons";
+import { SingleProductType } from "../context/products_context";
 
-const AddToCart = () => {
-  return <h4>addToCart </h4>;
+interface AddToCartProps {
+  product: SingleProductType;
+}
+
+const AddToCart = ({ product }: AddToCartProps) => {
+  const { id, stock, colors } = product;
+  const [mainColor, setMainColor] = useState(colors[0]);
+
+  return (
+    <Wrapper>
+      {" "}
+      <div className="colors">
+        <span>colors :</span>
+        <div>
+          {colors.map((color, index) => {
+            return (
+              <button
+                key={index}
+                style={{ background: color }}
+                className={`${
+                  mainColor === color ? "color-btn active" : "color-btn"
+                }`}
+                onClick={() => setMainColor(color)}
+              >
+                {mainColor === color ? <FaCheck /> : null}
+              </button>
+            );
+          })}
+        </div>
+      </div>{" "}
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.section`
