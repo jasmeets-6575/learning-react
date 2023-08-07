80c211ba5bd89165dc30d895b678c3c27960dcf2
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-08-07 08:54:16 +0530

    sort basics

--- by-john-smilga-ts/comfy-sloth/src/context/filter_context.tsx
+++ by-john-smilga-ts/comfy-sloth/src/context/filter_context.tsx
@@ -26,6 +26,8 @@ const initialState: FilterInitialStateType = {
 type IFilterAppState = {
   filtered_products: ProductType[];
   grid_view: boolean;
+  setGridView: () => void;
+  setListView: () => void;
 };
 
 const FilterContext = React.createContext<IFilterAppState>(
@@ -42,8 +44,15 @@ export const FilterProvider = ({ children }: ChildrenType): ReactElement => {
     dispatch({ type: LOAD_PRODUCTS, payload: products });
   }, [products]);
 
+  const setGridView = () => {
+    dispatch({ type: SET_GRIDVIEW });
+  };
+  const setListView = () => {
+    dispatch({ type: SET_LISTVIEW });
+  };
+
   return (
-    <FilterContext.Provider value={{ ...state }}>
+    <FilterContext.Provider value={{ ...state, setGridView, setListView }}>
       {children}
     </FilterContext.Provider>
   );
--- by-john-smilga-ts/comfy-sloth/src/reducers/filter_reducer.tsx
+++ by-john-smilga-ts/comfy-sloth/src/reducers/filter_reducer.tsx
@@ -27,6 +27,12 @@ const filter_reducer = (
   if (action.type === LOAD_PRODUCTS) {
     return { ...state, all_products: [...action.payload] };
   }
+  if (action.type === SET_GRIDVIEW) {
+    return { ...state, grid_view: true };
+  }
+  if (action.type === SET_LISTVIEW) {
+    return { ...state, grid_view: false };
+  }
   throw new Error(`No Matching action type`);
 };
 
