f870c37c83fe625c82fb0e0e0cefb49c8bf37cc9
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-08-08 16:20:00 +0530

    colors

--- by-john-smilga-ts/comfy-sloth/src/components/Filters.tsx
+++ by-john-smilga-ts/comfy-sloth/src/components/Filters.tsx
@@ -84,6 +84,45 @@ const Filters = () => {
             </select>
           </div>
           {/* end of company */}
+
+          {/* colors */}
+          <div className="form-control">
+            <h5>colors</h5>
+            <div className="colors">
+              {colors.map((c, index) => {
+                if (c === "all") {
+                  return (
+                    <button
+                      key={index}
+                      name="color"
+                      onClick={updateFilters}
+                      data-color="all"
+                      className={`${
+                        color === "all" ? "all-btn active" : "all-btn"
+                      }`}
+                    >
+                      all
+                    </button>
+                  );
+                }
+                return (
+                  <button
+                    key={index}
+                    name="color"
+                    style={{ background: c }}
+                    className={`${
+                      color === c ? "color-btn active" : "color-btn"
+                    }`}
+                    data-color={c}
+                    onClick={updateFilters}
+                  >
+                    {color === c ? <FaCheck /> : null}
+                  </button>
+                );
+              })}
+            </div>
+          </div>
+          {/* end of colors */}
         </form>
         <button type="button" className="clear-btn" onClick={clearFilters}>
           clear filters
