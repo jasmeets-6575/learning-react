d5bb2681ffac83416a4cd6e3fd8afe6305962e60
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-08-08 16:18:00 +0530

    company

--- by-john-smilga-ts/comfy-sloth/src/components/Filters.tsx
+++ by-john-smilga-ts/comfy-sloth/src/components/Filters.tsx
@@ -64,6 +64,26 @@ const Filters = () => {
             </div>
           </div>
           {/* end of category */}
+
+          {/* company */}
+          <div className="form-control">
+            <h5>company</h5>
+            <select
+              name="company"
+              value={company}
+              onChange={updateFilters}
+              className="company"
+            >
+              {companies.map((c, index) => {
+                return (
+                  <option key={index} value={c}>
+                    {c}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          {/* end of company */}
         </form>
         <button type="button" className="clear-btn" onClick={clearFilters}>
           clear filters
