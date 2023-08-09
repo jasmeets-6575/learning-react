7ec539f0953ede9ab19b113f7d28c4e83a0bb583
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-08-09 16:30:42 +0530

    remoevItem increase and decrease functionality

--- by-john-smilga-ts/redux-toolkit/src/components/CartItem.tsx
+++ by-john-smilga-ts/redux-toolkit/src/components/CartItem.tsx
@@ -1,21 +1,46 @@
 import { CartItemType } from "../cartItems";
+import { removeItem, increase, decrease } from "../features/cart/cartSlice";
+
 import { ChevronDown, ChevronUp } from "../icons";
+import { useAppDispatch } from "../store";
 
 const CartItem = ({ id, img, title, price, amount }: CartItemType) => {
+  const dispatch = useAppDispatch();
   return (
     <article className="cart-item">
       <img src={img} alt={title} />
       <div>
         <h4>{title}</h4>
         <h4 className="item-price">${price}</h4>
-        <button className="remove-btn">remove</button>
+        <button
+          className="remove-btn"
+          onClick={() => {
+            dispatch(removeItem(id));
+          }}
+        >
+          remove
+        </button>
       </div>
       <div>
-        <button className="amount-btn">
+        <button
+          className="amount-btn"
+          onClick={() => {
+            dispatch(increase({ id }));
+          }}
+        >
           <ChevronUp />
         </button>
         <p className="amount">{amount}</p>
-        <button className="amount-btn">
+        <button
+          className="amount-btn"
+          onClick={() => {
+            if (amount === 1) {
+              dispatch(removeItem(id));
+              return;
+            }
+            dispatch(decrease({ id }));
+          }}
+        >
           <ChevronDown />
         </button>
       </div>
