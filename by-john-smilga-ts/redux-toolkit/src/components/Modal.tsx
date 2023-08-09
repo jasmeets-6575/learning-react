0aa310b02724edbdc2badc440151eb095f375662
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-08-09 16:54:11 +0530

    adding modal

--- by-john-smilga-ts/redux-toolkit/src/components/Modal.tsx
+++ by-john-smilga-ts/redux-toolkit/src/components/Modal.tsx
@@ -1,4 +1,28 @@
+import { clearCart } from "../features/cart/cartSlice";
+import { useAppDispatch } from "../store";
+
 const Modal = () => {
-  return <div>Modal</div>;
+  const dispatch = useAppDispatch();
+  return (
+    <aside className="modal-container">
+      <div className="modal">
+        <h4>remove all items from your shopping cart?</h4>
+        <div className="btn-container">
+          <button
+            type="button"
+            className="btn confirm-btn"
+            onClick={() => {
+              dispatch(clearCart());
+            }}
+          >
+            confirm
+          </button>
+          <button type="button" className="btn clear-btn">
+            cancel
+          </button>
+        </div>
+      </div>
+    </aside>
+  );
 };
 export default Modal;
