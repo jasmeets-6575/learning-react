f82aaa638e0853c69ff1f58fb79bbaf42308e7e8
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-08-09 15:36:30 +0530

    setting navbar

--- by-john-smilga-ts/redux-toolkit/src/components/Navbar.tsx
+++ by-john-smilga-ts/redux-toolkit/src/components/Navbar.tsx
@@ -1,4 +1,22 @@
+import { CartIcon } from "../icons";
+import { useAppSelector } from "../store";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const { amount } = useAppSelector((store) => store.cart);
+  return (
+    <>
+      <nav>
+        <div className="nav-center">
+          <h3>redux toolkit</h3>
+          <div className="nav-container">
+            <CartIcon />
+            <div className="amount-container">
+              <p className="total-amount">{amount}</p>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 };
 export default Navbar;
