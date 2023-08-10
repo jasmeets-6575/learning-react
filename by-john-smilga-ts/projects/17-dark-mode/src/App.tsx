26aaaf21e8adbc288dda7a509953277195fb9739
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-08-10 15:04:33 +0530

    dark mode complete

--- by-john-smilga-ts/projects/17-dark-mode/src/App.tsx
+++ by-john-smilga-ts/projects/17-dark-mode/src/App.tsx
@@ -1,5 +1,48 @@
+import { useState, useEffect } from "react";
+import data, { ArticleType } from "./data";
+import Article from "./Article";
+
+const getStorageTheme = () => {
+  let theme = "light-theme";
+  let locSto = localStorage.getItem("theme");
+  if (locSto) {
+    theme = locSto;
+  }
+  return theme;
+};
+
 function App() {
-  return <h1>hello world</h1>;
+  const [theme, setTheme] = useState(getStorageTheme());
+  const toggleTheme = () => {
+    if (theme === "light-theme") {
+      setTheme("dark-theme");
+    } else {
+      setTheme("light-theme");
+    }
+  };
+
+  useEffect(() => {
+    document.documentElement.className = theme;
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  return (
+    <main>
+      <nav>
+        <div className="nav-center">
+          <h1>overreacted</h1>
+          <button className="btn" onClick={toggleTheme}>
+            toggle
+          </button>
+        </div>
+      </nav>
+      <section className="articles">
+        {data.map((item: ArticleType) => {
+          return <Article key={item.id} {...item} />;
+        })}
+      </section>
+    </main>
+  );
 }
 
 export default App;
