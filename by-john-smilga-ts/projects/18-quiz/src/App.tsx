0f32c5c077c39236694dd24761420010d75e13f3
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-08-10 19:25:03 +0530

    fetching data

--- by-john-smilga-ts/projects/18-quiz/src/App.tsx
+++ by-john-smilga-ts/projects/18-quiz/src/App.tsx
@@ -1,4 +1,16 @@
+import { useGlobalContext } from "./context";
+import SetupForm from "./setupForm";
+import Loading from "./Loading";
+import Modal from "./Modal";
+
 function App() {
+  const { waiting, loading, questions, index, correct } = useGlobalContext();
+  if (waiting) {
+    return <SetupForm />;
+  }
+  if (loading) {
+    return <Loading />;
+  }
   return <h1>Hello world</h1>;
 }
 
