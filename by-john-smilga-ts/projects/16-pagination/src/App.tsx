b432bef52d594144cfeee24028ee7b3b5b323c2c
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-08-10 12:40:50 +0530

    rendering data

--- by-john-smilga-ts/projects/16-pagination/src/App.tsx
+++ by-john-smilga-ts/projects/16-pagination/src/App.tsx
@@ -1,5 +1,50 @@
+import { useState, useEffect } from "react";
+import { useFetch } from "./useFetch";
+import Follower from "./Follower";
+
+interface IFollower {
+  id: number;
+  avatar_url: string;
+  html_url: string;
+  login: string;
+}
+
 function App() {
-  return <h1>Hello World</h1>;
+  const { loading, data } = useFetch();
+  const [page, setPage] = useState(0);
+  const [followers, setFollowers] = useState([]);
+
+  return (
+    <main>
+      <div className="section-title">
+        <h1>{loading ? "loading..." : "pagination"}</h1>
+        <div className="underline"></div>
+      </div>
+      <section className="followers">
+        <div className="container">
+          {followers.map((follower: IFollower) => {
+            return <Follower key={follower.id} {...follower} />;
+          })}
+        </div>
+        {!loading && (
+          <div className="btn-container">
+            <button className="prev-btn">prev</button>
+            {data.map((item, index) => {
+              return (
+                <button
+                  key={index}
+                  className={`page-btn ${index === page ? "active-btn" : null}`}
+                >
+                  {index + 1}
+                </button>
+              );
+            })}
+            <button className="next-btn">next</button>
+          </div>
+        )}
+      </section>
+    </main>
+  );
 }
 
 export default App;
--- by-john-smilga-ts/projects/16-pagination/src/Follower.tsx
+++ by-john-smilga-ts/projects/16-pagination/src/Follower.tsx
@@ -1,4 +1,18 @@
-const Follower = () => {
-  return <div>Follower</div>;
+interface FollowerProps {
+  avatar_url: string;
+  html_url: string;
+  login: string;
+}
+const Follower = ({ avatar_url, html_url, login }: FollowerProps) => {
+  return (
+    <article className="card">
+      <img src={avatar_url} alt={login} />
+      <h4>${login}</h4>
+      <a href={html_url} className="btn">
+        view profile
+      </a>
+    </article>
+  );
 };
+
 export default Follower;
