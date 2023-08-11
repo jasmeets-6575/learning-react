248a57499f69ce6c218fc2ec4b17c50edc82f859
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-08-11 11:18:30 +0530

    Landing Page

--- by-john-smilga-ts/jobster-app-ts/src/pages/Landing.tsx
+++ by-john-smilga-ts/jobster-app-ts/src/pages/Landing.tsx
@@ -1,4 +1,31 @@
+import main from "../assets/images/main.svg";
+import Wrapper from "../assets/wrappers/LandingPage";
+import { Logo } from "../components";
+import { Link } from "react-router-dom";
 const Landing = () => {
-  return <div>Landing</div>;
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app{" "}
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni,
+            voluptatem neque. Perferendis distinctio reiciendis iure corporis
+            laboriosam eligendi doloribus cum officiis natus optio, iusto saepe.
+          </p>
+          <Link to="/register" className="btn btn-hero">
+            Login / Register
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
 };
+
 export default Landing;
