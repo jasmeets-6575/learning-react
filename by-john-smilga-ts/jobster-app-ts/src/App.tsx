4d90834b198fe24e93c59016492897da5fd83b22
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-08-11 13:37:45 +0530

    setting react router

--- by-john-smilga-ts/jobster-app-ts/src/App.tsx
+++ by-john-smilga-ts/jobster-app-ts/src/App.tsx
@@ -1,5 +1,32 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Landing, Error, Register, ProtectedRoute } from "./pages";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import {
+  AllJobs,
+  Profile,
+  SharedLayout,
+  Stats,
+  AddJob,
+} from "./pages/dashboard";
+
 function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Stats />} />
+          <Route path="all-jobs" element={<AllJobs />} />
+          <Route path="add-job" element={<AddJob />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
+        <Route path="register" element={<Register />} />
+        <Route path="landing" element={<Landing />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <ToastContainer position="top-center" />
+    </BrowserRouter>
+  );
 }
 
 export default App;
