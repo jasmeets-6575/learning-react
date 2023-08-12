930af9b9bd5d167e9cd14814f9fd8e0f568a8ee5
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-08-12 19:44:19 +0530

    Profile setup

--- by-john-smilga-ts/jobster-app-ts/src/pages/dashboard/Profile.tsx
+++ by-john-smilga-ts/jobster-app-ts/src/pages/dashboard/Profile.tsx
@@ -1,4 +1,70 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+import { FormRow } from "../../components";
+import Wrapper from "../../assets/wrappers/DashboardFormPage";
+import { toast } from "react-toastify";
+import { useAppDispatch, useAppSelector } from "../../Store";
+
 const Profile = () => {
-  return <div>Profile</div>;
+  const { isLoading, user } = useAppSelector((store) => store.user);
+  const dispatch = useAppDispatch();
+  const [userData, setUserData] = useState({
+    name: user?.name || "",
+    email: user?.email || "",
+    lastName: user?.lastName || "",
+    location: user?.location || "",
+  });
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const { name, email, lastName, location } = userData;
+    if (!name || !email || !lastName || !location) {
+      toast.error("Please fill out all fields");
+      return;
+    }
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setUserData({ ...userData, [name]: value });
+  };
+
+  return (
+    <Wrapper>
+      <form className="form" onSubmit={handleSubmit}>
+        <h3>profile</h3>
+        <div className="form-center">
+          <FormRow
+            type="text"
+            name="name"
+            value={userData.name}
+            handleChange={handleChange}
+          />
+          <FormRow
+            type="text"
+            labelText="last name"
+            name="lastName"
+            value={userData.lastName}
+            handleChange={handleChange}
+          />
+          <FormRow
+            type="email"
+            name="email"
+            value={userData.email}
+            handleChange={handleChange}
+          />
+          <FormRow
+            type="text"
+            name="location"
+            value={userData.location}
+            handleChange={handleChange}
+          />
+          <button className="btn btn-block" type="submit" disabled={isLoading}>
+            {isLoading ? "Please Wait..." : "save changes"}
+          </button>
+        </div>
+      </form>
+    </Wrapper>
+  );
 };
 export default Profile;
--- by-john-smilga-ts/jobster-app-ts/src/pages/dashboard/SharedLayout.tsx
+++ by-john-smilga-ts/jobster-app-ts/src/pages/dashboard/SharedLayout.tsx
@@ -1,4 +1,25 @@
-const SharedLayout = () => {
-  return <div>SharedLayout</div>;
+import React from "react";
+import { Outlet } from "react-router-dom";
+import { BigSidebar, Navbar, SmallSidebar } from "../../components";
+import Wrapper from "../../assets/wrappers/SharedLayout";
+
+const SharedLayout: React.FC = () => {
+  return (
+    <>
+      <Wrapper>
+        <main className="dashboard">
+          <SmallSidebar />
+          <BigSidebar />
+          <div>
+            <Navbar />
+            <div className="dashboard-page">
+              <Outlet />
+            </div>
+          </div>
+        </main>
+      </Wrapper>
+    </>
+  );
 };
+
 export default SharedLayout;
