ac1c0efbc4b1e6d4465089ba519f57990a53c0dd
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-08-12 09:07:15 +0530

    FormRow component

--- by-john-smilga-ts/jobster-app-ts/src/components/FormRow.tsx
+++ by-john-smilga-ts/jobster-app-ts/src/components/FormRow.tsx
@@ -1,4 +1,34 @@
-const FormRow = () => {
-  return <div>FormRow</div>;
+import React, { ChangeEvent } from "react";
+
+interface FormRowProps {
+  type: string;
+  name: string;
+  value: string;
+  handleChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  labelText?: string;
+}
+
+const FormRow: React.FC<FormRowProps> = ({
+  type,
+  name,
+  value,
+  handleChange,
+  labelText,
+}) => {
+  return (
+    <div className="form-row">
+      <label htmlFor={name} className="form-label">
+        {labelText || name}
+      </label>
+      <input
+        id={name}
+        type={type}
+        name={name}
+        value={value}
+        onChange={handleChange}
+        className="form-input"
+      />
+    </div>
+  );
 };
 export default FormRow;
--- by-john-smilga-ts/jobster-app-ts/src/pages/Register.tsx
+++ by-john-smilga-ts/jobster-app-ts/src/pages/Register.tsx
@@ -1,4 +1,20 @@
+import { useState, useEffect } from "react";
+import { FormRow, Logo } from "../components";
+import Wrapper from "../assets/wrappers/RegisterPage";
+import { toast } from "react-toastify";
+import { useDispatch, useSelector } from "react-redux";
+// import { loginUser, registerUser } from "../features/user/userSlice";
+import { useNavigate } from "react-router-dom";
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+};
+
 const Register = () => {
+  const [values, setValues] = useState(initialState);
   return <div>Register</div>;
 };
 export default Register;
