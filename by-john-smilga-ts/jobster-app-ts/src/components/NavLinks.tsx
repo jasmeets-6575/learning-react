29ec0687d3d068ca1b987d8e3085ff2dfff7d6e7
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-08-12 15:56:21 +0530

    smallSidebar

--- by-john-smilga-ts/jobster-app-ts/src/components/NavLinks.tsx
+++ by-john-smilga-ts/jobster-app-ts/src/components/NavLinks.tsx
@@ -1,4 +1,26 @@
-const NavLinks = () => {
-  return <div>NavLinks</div>;
+import { NavLink } from "react-router-dom";
+import links from "../utils/links";
+
+const NavLinks = ({ toggleSidebar }: { toggleSidebar: () => void }) => {
+  return (
+    <div className="nav-links">
+      {links.map((link) => {
+        const { text, path, id, icon } = link;
+        return (
+          <NavLink
+            key={id}
+            to={path}
+            className={({ isActive }) => {
+              return isActive ? "nav-link active" : "nav-link";
+            }}
+            onClick={toggleSidebar}
+          >
+            <span className="icon">{icon}</span>
+            {text}
+          </NavLink>
+        );
+      })}
+    </div>
+  );
 };
 export default NavLinks;
--- by-john-smilga-ts/jobster-app-ts/src/components/SmallSidebar.tsx
+++ by-john-smilga-ts/jobster-app-ts/src/components/SmallSidebar.tsx
@@ -1,4 +1,36 @@
+import Wrapper from "../assets/wrappers/SmallSidebar";
+import { FaTimes } from "react-icons/fa";
+import NavLinks from "./NavLinks";
+import Logo from "./Logo";
+import { toggleSidebar } from "../features/user/userSlice";
+import { useAppDispatch, useAppSelector } from "../Store";
+
 const SmallSidebar = () => {
-  return <div>SmallSidebar</div>;
+  const { isSidebarOpen } = useAppSelector((store) => store.user);
+  const dispatch = useAppDispatch();
+
+  const toggle = () => {
+    dispatch(toggleSidebar());
+  };
+  return (
+    <Wrapper>
+      <div
+        className={
+          isSidebarOpen ? "sidebar-container show-sidebar" : "sidebar-container"
+        }
+      >
+        <div className="content">
+          <button className="close-btn" onClick={toggle}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+
+          <NavLinks toggleSidebar={toggle} />
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 export default SmallSidebar;
