e59dcc27d025db684f396f1f576b7d0e708d56c2
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-08-12 09:17:30 +0530

    Register Setup

--- by-john-smilga-ts/jobster-app-ts/src/pages/Register.tsx
+++ by-john-smilga-ts/jobster-app-ts/src/pages/Register.tsx
@@ -1,10 +1,8 @@
-import { useState, useEffect } from "react";
+import { useState, ChangeEvent, FormEvent } from "react";
 import { FormRow, Logo } from "../components";
 import Wrapper from "../assets/wrappers/RegisterPage";
 import { toast } from "react-toastify";
-import { useDispatch, useSelector } from "react-redux";
 // import { loginUser, registerUser } from "../features/user/userSlice";
-import { useNavigate } from "react-router-dom";
 
 const initialState = {
   name: "",
@@ -15,6 +13,62 @@ const initialState = {
 
 const Register = () => {
   const [values, setValues] = useState(initialState);
-  return <div>Register</div>;
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setValues({ ...values, [name]: value });
+  };
+
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const { name, email, password, isMember } = values;
+    if (!email || !password || (!isMember && !name)) {
+      toast.error("please fill out all fields");
+      return;
+    }
+  };
+
+  const toggleMember = () => {
+    setValues({ ...values, isMember: !values.isMember });
+  };
+  return (
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={onSubmit}>
+        <Logo />
+        <h3>{values.isMember ? "Login" : "Register"}</h3>
+
+        {!values.isMember && (
+          <FormRow
+            type="text"
+            name="name"
+            value={values.name}
+            handleChange={handleChange}
+          />
+        )}
+
+        <FormRow
+          type="email"
+          name="email"
+          value={values.email}
+          handleChange={handleChange}
+        />
+
+        <FormRow
+          type="password"
+          name="password"
+          value={values.password}
+          handleChange={handleChange}
+        />
+
+        <p>
+          {values.isMember ? "Not a member yet?" : "Already a member?"}
+          <button type="button" onClick={toggleMember} className="member-btn">
+            {values.isMember ? "Register" : "Login"}
+          </button>
+        </p>
+      </form>
+    </Wrapper>
+  );
 };
 export default Register;
