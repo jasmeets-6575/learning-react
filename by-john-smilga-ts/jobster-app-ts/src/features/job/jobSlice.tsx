5136c9659e8c290949f45cb739c957663dfc4b11
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-08-13 18:42:30 +0530

    adding jobslice

--- by-john-smilga-ts/jobster-app-ts/src/features/job/jobSlice.tsx
+++ by-john-smilga-ts/jobster-app-ts/src/features/job/jobSlice.tsx
@@ -1,7 +1,30 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-type JobInitialStateType = {};
-const initialState: JobInitialStateType = {};
+type JobInitialStateType = {
+  isLoading: boolean;
+  position: string;
+  company: string;
+  jobLocation: string;
+  jobTypeOptions: string[];
+  jobType: string;
+  statusOptions: string[];
+  status: string;
+  isEditing: boolean;
+  editJobId: string;
+};
+
+const initialState: JobInitialStateType = {
+  isLoading: false,
+  position: "",
+  company: "",
+  jobLocation: "",
+  jobTypeOptions: ["full-time", "part-time", "remote", "internship"],
+  jobType: "full-time",
+  statusOptions: ["interview", "declined", "pending"],
+  status: "pending",
+  isEditing: false,
+  editJobId: "",
+};
 
 const jobSlice = createSlice({
   name: "job",
