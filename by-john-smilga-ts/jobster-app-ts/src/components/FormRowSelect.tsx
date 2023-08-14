402d022c5bbaedd7840e9b62974582bde445b13b
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-08-14 08:23:46 +0530

    setting FormRowSelect

--- by-john-smilga-ts/jobster-app-ts/src/components/FormRowSelect.tsx
+++ by-john-smilga-ts/jobster-app-ts/src/components/FormRowSelect.tsx
@@ -1,4 +1,41 @@
-const FormRowSelect = () => {
-  return <div>FormRowSelect</div>;
+type FormRowSelectProps = {
+  labelText?: string;
+  name: string;
+  value: string;
+  handleChange: (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => void;
+  list: string[];
+};
+
+const FormRowSelect = ({
+  labelText,
+  name,
+  value,
+  handleChange,
+  list,
+}: FormRowSelectProps) => {
+  return (
+    <div className="form-row">
+      <label htmlFor={name} className="form-label">
+        {labelText || name}
+      </label>
+      <select
+        name={name}
+        id={name}
+        value={value}
+        onChange={handleChange}
+        className="form-select"
+      >
+        {list.map((itemValue, index) => {
+          return (
+            <option key={index} value={itemValue}>
+              {itemValue}
+            </option>
+          );
+        })}
+      </select>
+    </div>
+  );
 };
 export default FormRowSelect;
